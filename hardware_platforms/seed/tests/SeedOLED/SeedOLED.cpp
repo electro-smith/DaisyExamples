5eaeea315d239f4fb65ba5958b9bf1e13a638479
Stephen Hensley <stephen.p.hensley@gmail.com>
2020-03-19 10:22:09 -0700

    reformatted oled example

--- hardware_platforms/seed/tests/SeedOLED/SeedOLED.cpp
+++ hardware_platforms/seed/tests/SeedOLED/SeedOLED.cpp
@@ -14,28 +14,28 @@ uint8_t message_idx;
 
 int     main(void)
 {
-    daisy_seed_init(&hw);
-    display.Init();
-    state = true;
-    xcnt = ycnt = 0;
-    message_idx = 0;
-    char strbuff[128];
-    while(1)
-    {
-               dsy_system_delay(500);
-        switch(message_idx)
-        {
-            case 0: sprintf(strbuff, "Testing. . ."); break;
-            case 1: sprintf(strbuff, "Daisy. . ."); break;
-            case 2: sprintf(strbuff, "1. . ."); break;
-            case 3: sprintf(strbuff, "2. . ."); break;
-            case 4: sprintf(strbuff, "3. . ."); break;
-            default: break;
-        }
-        message_idx = (message_idx + 1) % 5;
-        display.Fill(false);
-        display.SetCursor(0, 0);
-        display.WriteString(strbuff, Font_11x18, true);
-        display.Update();
-    }
+	daisy_seed_init(&hw);
+	display.Init();
+	state = true;
+	xcnt = ycnt = 0;
+	message_idx = 0;
+	char strbuff[128];
+	while (1)
+	{
+		dsy_system_delay(500);
+		switch (message_idx)
+		{
+		case 0: sprintf(strbuff, "Testing. . ."); break;
+		case 1: sprintf(strbuff, "Daisy. . ."); break;
+		case 2: sprintf(strbuff, "1. . ."); break;
+		case 3: sprintf(strbuff, "2. . ."); break;
+		case 4: sprintf(strbuff, "3. . ."); break;
+		default: break;
+		}
+		message_idx = (message_idx + 1) % 5;
+		display.Fill(false);
+		display.SetCursor(0, 0);
+		display.WriteString(strbuff, Font_11x18, true);
+		display.Update();
+	}
 }
