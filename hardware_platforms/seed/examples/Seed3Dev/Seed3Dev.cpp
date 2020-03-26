0d2cb422b6a1c941b09eeefc7c44a538823ba188
Stephen Hensley <stephen.p.hensley@gmail.com>
2020-03-26 16:31:32 -0700

    current state of tests

--- hardware_platforms/seed/examples/Seed3Dev/Seed3Dev.cpp
+++ hardware_platforms/seed/examples/Seed3Dev/Seed3Dev.cpp
@@ -5,15 +5,24 @@
 #include <stdio.h>
 #include <string.h>
 //#include "daisy_seed.h"
-//#include "daisy_pod.h"
+#include "daisy_pod.h"
 #include "daisy_petal.h"
 #include "daisysp.h"
 using namespace daisy;
 
+//#define USE_POD
+
 // Globals
-DaisyPetal          hw;
-daisysp::Oscillator DSY_SDRAM_BSS osc;
-uint8_t                           wave;
+#ifdef USE_POD
+#define DEV_NAMESPACE DaisyPod
+#else
+#define DEV_NAMESPACE DaisyPetal
+#endif
+DEV_NAMESPACE hw;
+//daisysp::Oscillator DSY_SDRAM_BSS osc;
+daisysp::Oscillator osc;
+daisysp::DelayLine<float, 48000 * 4> DSY_SDRAM_BSS del;
+uint8_t             wave;
 
 float amp, targetamp;
 
@@ -23,7 +32,7 @@ void AudioTest(float *in, float *out, size_t size)
     hw.DebounceControls();
     float sig, note;
     // One way to get value
-    note = hw.GetKnobValue(DaisyPetal::KNOB_6) * 127.0f;
+    note = hw.GetKnobValue(DEV_NAMESPACE::KNOB_1) * 127.0f;
     osc.SetFreq(daisysp::mtof(note));
 
     // Handle Encoder for waveform switching test.
@@ -42,26 +51,40 @@ void AudioTest(float *in, float *out, size_t size)
     }
 
     // Turn amplitude on when pressing button.
-    targetamp
-        = (hw.encoder.Pressed() || hw.switches[DaisyPetal::SW_5].Pressed())
-              ? 1.0f
-              : 0.0f;
+    //    targetamp
+    //        = (hw.encoder.Pressed() || hw.switches[DEV_NAMESPACE::SW_5].Pressed())
+    //              ? 0.8f
+    //              : 0.0f;
+    targetamp = (hw.encoder.Pressed()) ? 0.8f : 0.0f;
 
     for(size_t i = 0; i < size; i += 2)
 
     {
         daisysp::fonepole(amp, targetamp, 0.005f);
         osc.SetAmp(amp);
-        if(hw.switches[DaisyPetal::SW_6].Pressed())
-        {
-            sig    = osc.Process();
-            out[i] = out[i + 1] = sig;
-        }
-        else
-        {
-            out[i]     = in[i];
-            out[i + 1] = in[i + 1];
-        }
+//        sig    = osc.Process();
+//        out[i] = out[i + 1] = sig;
+                if(hw.switches[DEV_NAMESPACE::SW_6].Pressed())
+                {
+                    sig    = osc.Process();
+                    out[i] = out[i + 1] = sig;
+                }
+                else
+                {
+                    float wetl, wetr, dryl, dryr, sendl, sendr;
+                    wetl  = 0.0f;
+                    wetr  = 0.0f;
+                    dryl = in[i];
+                    dryr = in[i + 1];
+                    //                    verb.Process(sendl, sendr, &wetl, &wetr);
+                    wetl = del.Read();
+                    sendl = dryl + (wetl * 0.5f);
+                    sendr = dryr + (wetr * 0.5f);
+                    del.Write(sendl);
+
+                    out[i]     = dryl + wetl;
+                    out[i + 1] = dryr + wetr;
+                }
     }
 }
 
@@ -73,8 +96,12 @@ int   main(void)
     uint8_t  active_led;
     hw.Init();
     samplerate = hw.AudioSampleRate();
+    hw.SetAudioBlockSize(128);
     // Init Synthesis
     osc.Init(samplerate);
+    del.Init();
+    del.SetDelay(samplerate * 0.25f);
+    //    verb.Init(samplerate);
     // Start
     hw.StartAdc();
     hw.StartAudio(AudioTest);
@@ -83,35 +110,35 @@ int   main(void)
     {
         now = dsy_system_getnow();
 
-        r        = hw.GetKnobValue(DaisyPetal::KNOB_1);
-        g        = hw.GetKnobValue(DaisyPetal::KNOB_2);
-        b        = hw.GetKnobValue(DaisyPetal::KNOB_3);
-        fsbright = hw.GetKnobValue(DaisyPetal::KNOB_4);
+//        r        = hw.GetKnobValue(DEV_NAMESPACE::KNOB_1);
+//        g        = hw.GetKnobValue(DEV_NAMESPACE::KNOB_2);
+//        b        = hw.GetKnobValue(DEV_NAMESPACE::KNOB_3);
+//        fsbright = hw.GetKnobValue(DEV_NAMESPACE::KNOB_4);
+//
+//        // Led Ring goes in a circle
+//        if(now - tick > 250)
+//        {
+//            tick       = now;
+//            active_led = (active_led + 1) % DEV_NAMESPACE::RING_LED_LAST;
+//        }
+//        for(size_t i = 0; i < DEV_NAMESPACE::RING_LED_LAST; i++)
+//        {
+//            if(i == active_led)
+//            {
+//                hw.SetRingLed(static_cast<DEV_NAMESPACE::RingLed>(i), r, g, b);
+//            }
+//            else
+//            {
+//                hw.SetRingLed(
+//                    static_cast<DEV_NAMESPACE::RingLed>(i), 0.0f, 0.0f, 0.0f);
+//            }
+//        }
+//        hw.SetFootswitchLed(DEV_NAMESPACE::FOOTSWITCH_LED_1, fsbright);
+//        hw.SetFootswitchLed(DEV_NAMESPACE::FOOTSWITCH_LED_2, fsbright);
+//        hw.SetFootswitchLed(DEV_NAMESPACE::FOOTSWITCH_LED_3, fsbright);
+//        hw.SetFootswitchLed(DEV_NAMESPACE::FOOTSWITCH_LED_4, fsbright);
 
-        // Led Ring goes in a circle
-        if(now - tick > 250)
-        {
-            tick       = now;
-            active_led = (active_led + 1) % DaisyPetal::RING_LED_LAST;
-        }
-        for(size_t i = 0; i < DaisyPetal::RING_LED_LAST; i++)
-        {
-            if(i == active_led)
-            {
-                hw.SetRingLed(static_cast<DaisyPetal::RingLed>(i), r, g, b);
-            }
-            else
-            {
-                hw.SetRingLed(
-                    static_cast<DaisyPetal::RingLed>(i), 0.0f, 0.0f, 0.0f);
-            }
-        }
-        hw.SetFootswitchLed(DaisyPetal::FOOTSWITCH_LED_1, fsbright);
-        hw.SetFootswitchLed(DaisyPetal::FOOTSWITCH_LED_2, fsbright);
-        hw.SetFootswitchLed(DaisyPetal::FOOTSWITCH_LED_3, fsbright);
-        hw.SetFootswitchLed(DaisyPetal::FOOTSWITCH_LED_4, fsbright);
-
-        hw.UpdateLeds();
-        hw.DelayMs(20);
+        //        hw.UpdateLeds();
+//        hw.DelayMs(20);
     }
 }
