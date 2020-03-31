6fc12eba50348a88681331f5a7465358270fb859
stephenhensley <shensley.audio@gmail.com>
2020-03-31 15:23:22 -0700

    fixed to actually do LED stuff again.

--- hardware_platforms/pod/examples/encoder/main.cpp
+++ hardware_platforms/pod/examples/encoder/main.cpp
@@ -6,6 +6,10 @@ static DaisyPod hw;
 static uint8_t led_sel;
 static int32_t inc;
 
+#define NUM_COLORS 5
+
+Color my_colors[5];
+
 static void callback(float *in, float *out, size_t size)
 {
     // Debounce the encoder at a steady, fixed rate.
@@ -16,7 +20,7 @@ static void callback(float *in, float *out, size_t size)
     {
         led_sel += 1;
         // Wrap around
-        if (led_sel > DaisyPod::LED_LAST - 1)
+        if (led_sel > NUM_COLORS - 1)
         {
             led_sel = 0;
         }
@@ -26,7 +30,7 @@ static void callback(float *in, float *out, size_t size)
         // Wrap around
         if (led_sel == 0)
         {
-            led_sel = DaisyPod::LED_LAST - 1;
+            led_sel = NUM_COLORS - 1;
         }
         else
         {
@@ -35,8 +39,12 @@ static void callback(float *in, float *out, size_t size)
     }
     if (hw.encoder.RisingEdge())
     {
-        led_sel = 0;
+        led_sel = 4;
     }
+    hw.ClearLeds();
+    hw.led1.SetColor(my_colors[led_sel]);
+    hw.led2.SetColor(my_colors[led_sel]);
+    hw.UpdateLeds();
 }
 
 int main(void)
@@ -44,11 +52,16 @@ int main(void)
     hw.Init();
     led_sel = 0;
     inc = 0;
+    my_colors[0].Init(Color::PresetColor::RED);
+    my_colors[1].Init(Color::PresetColor::GREEN);
+    my_colors[2].Init(Color::PresetColor::BLUE);
+    my_colors[3].Init(Color::PresetColor::WHITE);
+    my_colors[4].Init(Color::PresetColor::OFF);
     // until we have fixed timer callbacks we'll use the audio callback
     hw.StartAudio(callback);
     while(1) 
     { 
-        hw.ClearLeds();
-        hw.SetLed(static_cast<DaisyPod::Led>(led_sel), 1);
+        //hw.ClearLeds();
+        //hw.SetLed(static_cast<DaisyPod::Led>(led_sel), 1);
     }
 }
