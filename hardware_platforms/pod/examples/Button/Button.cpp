e877d7df9203bbbfaf62abc4f362436212fcc895
aikenberry <andrew@qubitelectronix.com>
2020-05-19 06:39:09 -0700

    added delays and moved led updates to callback to no avail

--- hardware_platforms/pod/examples/Button/Button.cpp
+++ hardware_platforms/pod/examples/Button/Button.cpp
@@ -3,43 +3,69 @@
 using namespace daisy;
 
 DaisyPod hw;
+bool     on = 0;
 
+static void callback(float *in, float *out, size_t size)
+{
+    hw.DebounceControls();
+
+    // using button1 as momentary switch for turning on/off led1
+    if(hw.button1.Pressed())
+    {
+        hw.led1.Set(.5, .5, .5);
+    }
+    else
+    {
+        hw.led1.Set(0, 0, 0);
+    }
+
+    // using button2 as latching switch for toggling led2
+    if(hw.button2.RisingEdge())
+    {
+        if(on)
+            hw.led2.Set(0, 0, 0);
+        else
+            hw.led2.Set(.5, .5, .5);
+
+        on = !on;
+    }
+    hw.UpdateLeds();
+    
+    for(size_t i = 0; i < size; i += 2)
+    {
+        // Process audio
+    }
+}
 int main(void)
 {
     hw.Init();
-    int  flag = 0;
-    bool on   = 0;
+    hw.StartAudio(callback);
 
     while(1)
     {
-        hw.DebounceControls();
-
-        // using button1 as momentary switch for turning on/off led1
-        if(hw.button1.Pressed())
-        {
-            hw.led1.Set(.5, .5, .5);
-        }
-        else
-        {
-            hw.led1.Set(0, 0, 0);
-        }
-
-        // using button2 as latching switch for toggling led2
-        if(hw.button2.Pressed() && flag == 0)
-        {
-            if(on)
-                hw.led2.Set(0, 0, 0);
-            else
-                hw.led2.Set(.5, .5, .5);
-
-            flag = 1;
-            on   = !on;
-        }
-        else
-        {
-            flag = 0;
-        }
-
-        hw.UpdateLeds();
+        //        hw.DebounceControls();
+        //
+        //        // using button1 as momentary switch for turning on/off led1
+        //        if(hw.button1.Pressed())
+        //        {
+        //            hw.led1.Set(.5, .5, .5);
+        //        }
+        //        else
+        //        {
+        //            hw.led1.Set(0, 0, 0);
+        //        }
+        //
+        //        // using button2 as latching switch for toggling led2
+        //        if(hw.button2.RisingEdge())
+        //        {
+        //            if(on)
+        //                hw.led2.Set(0, 0, 0);
+        //            else
+        //                hw.led2.Set(.5, .5, .5);
+        //
+        //            on = !on;
+        //        }
+        //        hw.UpdateLeds();
+        //        //hw.DelayMs(10);
     }
 }
