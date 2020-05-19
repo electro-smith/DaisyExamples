85d37624d1b52f30b34dc3c902b8541fb0d36ca0
aikenberry <andrew@qubitelectronix.com>
2020-05-19 07:18:20 -0700

    added waveform and octave controls

--- hardware_platforms/pod/examples/oscillator/oscillator.cpp
+++ hardware_platforms/pod/examples/oscillator/oscillator.cpp
@@ -6,34 +6,58 @@ using namespace daisysp;
 
 DaisyPod   hw;
 Oscillator osc;
-parameter  p_freq;
+Parameter  p_freq;
+
+uint8_t waveforms[4] = {
+    Oscillator::WAVE_SIN,
+    Oscillator::WAVE_TRI,
+    Oscillator::WAVE_POLYBLEP_SAW,
+    Oscillator::WAVE_POLYBLEP_SQUARE,
+};
+uint8_t octave_factor[5] = {
+    1,
+    2,
+    4,
+    8,
+    16,
+};
 
 static float   freq;
-float          sig, waveform;
-static int32_t inc, octave = 2;
+float          sig;
+static int32_t inc, waveform = 0, octave = 0;
 
 static void audioCallback(float *in, float *out, size_t size)
 {
-    hw.encoder.Debounce();
+    hw.DebounceControls();
     inc = hw.encoder.Increment();
     if(inc > 0)
     {
-        octave *= 2;
-        if(octave >= 10)
-            octave = 10;
+        waveform++;
+        if(waveform > 3)
+            waveform = 3;
     }
-
     if(inc < 0)
     {
-        octave *= .5;
-        if(octave <= 2)
-            octave = 2;
+        waveform--;
+        if(waveform < 0)
+            waveform = 0;
     }
-    
-    // Get Parameters
-    freq = p_freq.process();
+    osc.SetWaveform(waveforms[waveform]);
 
-    osc.SetFreq(freq*octave);
+    if(hw.button2.RisingEdge())
+    {
+        octave++;
+        if(octave > 4)
+            octave = 4;
+    }
+    if(hw.button1.RisingEdge())
+    {
+        octave--;
+        if(octave < 0)
+            octave = 0;
+    }
+    freq = p_freq.Process();
+    osc.SetFreq(freq*octave_factor[octave]);
 
     // Audio Loop
     for(size_t i = 0; i < size; i += 2)
@@ -48,11 +72,11 @@ static void audioCallback(float *in, float *out, size_t size)
 void InitSynth(float samplerate)
 {
     // Parameters
-    p_freq.init(hw.knob1, 10.0f, 12000.0f, parameter::LOG);
+    p_freq.Init(hw.knob1, 10.0f, 12000.0f, Parameter::LOGARITHMIC);
 
     // Init Osc
     osc.Init(samplerate);
-    osc.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW);
+    osc.SetWaveform(Oscillator::WAVE_SIN);
     osc.SetFreq(100.0f);
     osc.SetAmp(0.5f);
 }
