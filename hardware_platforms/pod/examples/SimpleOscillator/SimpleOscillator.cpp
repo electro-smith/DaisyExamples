ed46c618fc3ce2fec8b5df69f2502bc52b16ea34
andrewikenberry <andrewikenberry@gmail.com>
2020-05-20 15:16:34 -0700

    shortened waveform inc code, made num waveforms macro, deleted osc init defaults, fixed callback naming convention mismatch

--- hardware_platforms/pod/examples/SimpleOscillator/SimpleOscillator.cpp
+++ hardware_platforms/pod/examples/SimpleOscillator/SimpleOscillator.cpp
@@ -1,6 +1,8 @@
 #include "daisy_pod.h"
 #include "daisysp.h"
 
+#define NUM_WAVEFORMS 4
+
 using namespace daisy;
 using namespace daisysp;
 
@@ -8,7 +10,7 @@ DaisyPod   hw;
 Oscillator osc;
 Parameter  p_freq;
 
-uint8_t waveforms[4] = {
+uint8_t waveforms[NUM_WAVEFORMS] = {
     Oscillator::WAVE_SIN,
     Oscillator::WAVE_TRI,
     Oscillator::WAVE_POLYBLEP_SAW,
@@ -24,40 +26,42 @@ uint8_t octave_factor[5] = {
 
 static float   freq;
 float          sig;
-static int32_t inc, waveform = 0, octave = 0;
+static int32_t waveform = 0, octave = 0;
 
-static void audioCallback(float *in, float *out, size_t size)
+static void AudioCallback(float *in, float *out, size_t size)
 {
     hw.DebounceControls();
-    inc = hw.encoder.Increment();
-    if(inc > 0)
-    {
-        waveform++;
-        if(waveform > 3)
-            waveform = 3;
-    }
-    if(inc < 0)
-    {
-        waveform--;
-        if(waveform < 0)
-            waveform = 0;
-    }
+
+    // read state from encoder and increment waveform accordingly
+    waveform += hw.encoder.Increment();
+
+    // clamp if waveform exceeds limits
+    if(waveform >= NUM_WAVEFORMS)
+        waveform = NUM_WAVEFORMS - 1;
+    else if(waveform <= 0)
+        waveform = 0;
+
     osc.SetWaveform(waveforms[waveform]);
 
+    // if button2 has been pressed, octave up
     if(hw.button2.RisingEdge())
     {
         octave++;
         if(octave > 4)
             octave = 4;
     }
+
+    // if button1 has been pressed, octave down
     if(hw.button1.RisingEdge())
     {
         octave--;
         if(octave < 0)
             octave = 0;
     }
+
+    // assign freq value from pot
     freq = p_freq.Process();
-    osc.SetFreq(freq*octave_factor[octave]);
+    osc.SetFreq(freq * octave_factor[octave]);
 
     // Audio Loop
     for(size_t i = 0; i < size; i += 2)
@@ -71,25 +75,26 @@ static void audioCallback(float *in, float *out, size_t size)
 
 void InitSynth(float samplerate)
 {
-    // Parameters
+    // init freq parameter to knob1, min 10, max 12k, curve log
     p_freq.Init(hw.knob1, 10.0f, 12000.0f, Parameter::LOGARITHMIC);
 
-    // Init Osc
+    // init Osc
     osc.Init(samplerate);
-    osc.SetWaveform(Oscillator::WAVE_SIN);
-    osc.SetFreq(100.0f);
     osc.SetAmp(0.5f);
 }
 
 int main(void)
 {
     float samplerate;
-    // Init
+
+    // init everything
     hw.Init();
     samplerate = hw.AudioSampleRate();
     InitSynth(samplerate);
-    // Start Callbacks
+
+    // start callbacks
     hw.StartAdc();
-    hw.StartAudio(audioCallback);
+    hw.StartAudio(AudioCallback);
+
     while(1) {}
 }
