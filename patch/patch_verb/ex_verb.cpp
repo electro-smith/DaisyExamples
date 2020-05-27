0e35dd2122a7ba4f80e15ce8c34dc8c449ce7457
stephenhensley <shensley.audio@gmail.com>
2020-05-26 20:53:02 -0700

    fixed for DaisyPatch object.

--- patch/patch_verb/ex_verb.cpp
+++ patch/patch_verb/ex_verb.cpp
@@ -2,43 +2,56 @@
 #include "daisy_patch.h"
 using namespace daisy;
 using namespace daisysp;
-static daisy_patch patch;
+static DaisyPatch patch;
 static ReverbSc verb;
 static DcBlock blk[2];
-//static dsy_dcblock dcblock[2];
 static float drylevel, send;
 
-static void VerbCallback(float *in, float *out, size_t size)
+static void VerbCallback(float **in, float **out, size_t size)
 {
     float dryL, dryR, wetL, wetR, sendL, sendR;
-    for (size_t i = 0; i < size; i += 2)
+    patch.UpdateAnalogControls();
+    for (size_t i = 0; i < size; i++)
     {
-        //verb.SetFeedback(0.15f + (dsy_adc_get_float(KNOB_1) * 0.85f));
-        //verb.SetLpFreq(200.0f + (dsy_adc_get_float(KNOB_2) * 18000.0f));
-        drylevel = dsy_adc_get_float(daisy_patch::KNOB_3);
-        send = dsy_adc_get_float(daisy_patch::KNOB_4);
-        dryL = in[i];
-        dryR = in[i+1];
+        // read some controls
+        drylevel = patch.GetCtrlValue(patch.CTRL_1);
+        send     = patch.GetCtrlValue(patch.CTRL_2);
+
+        // Read Inputs (only stereo in are used)
+        dryL = in[0][i];
+        dryR = in[1][i];
+
+        // Send Signal to Reverb
         sendL = dryL * send;
         sendR = dryR * send;
         verb.Process(sendL, sendR, &wetL, &wetR);
+
+        // Dc Block
         wetL = blk[0].Process(wetL);
         wetR = blk[1].Process(wetR);
-        out[i] = (dryL * drylevel) + wetL;
-        out[i + 1] = (dryR * drylevel) + wetR;
+
+        // Out 1 and 2 are Mixed 
+        out[0][i] = (dryL * drylevel) + wetL;
+        out[1][i] = (dryR * drylevel) + wetR;
+
+        // Out 3 and 4 are just wet
+        out[2][i] = wetL;
+        out[3][i] = wetR;
     }
 }
 
 int main(void)
 {
+    float samplerate;
     patch.Init();
+    samplerate = patch.AudioSampleRate();
 
-    verb.Init(SAMPLE_RATE);
+    verb.Init(samplerate);
     verb.SetFeedback(0.85f);
     verb.SetLpFreq(18000.0f);
 
-    blk[0].Init(SAMPLE_RATE);
-    blk[1].Init(SAMPLE_RATE);
+    blk[0].Init(samplerate);
+    blk[1].Init(samplerate);
 
     dsy_adc_start();
     patch.StartAudio(VerbCallback);
