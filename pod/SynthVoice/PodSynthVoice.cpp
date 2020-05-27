ffe3355a6f86ee1f545b006decc4d40bc5be7606
beserge <bensergentanis@gmail.com>
2020-05-26 20:44:23 -0400

    added vibrato and mode LEDs

--- pod/SynthVoice/PodSynthVoice.cpp
+++ pod/SynthVoice/PodSynthVoice.cpp
@@ -5,17 +5,22 @@ using namespace daisysp;
 using namespace daisy;
 
 static DaisyPod pod;
-static Oscillator osc;
+static Oscillator osc, lfo;
 static Tone flt;
 static AdEnv ad;
-static Parameter pitch, cutoff;
+static Parameter pitch, cutoff, lfof;
 
 int wave, mode;
+float v, p;
 
+
+// Simple Synth voice example. Encoder cycles through three modes. Mode one controls cutoff and oscillator frequency. Mode 2 controls attack and decay times. Mode 3 controls vibrato rate and depth.
+// Click encoder to change waveform. Press either button to trigger the envelope.
+// Leds light to indicate what mode is selected.
 
 static void AudioCallback(float *in, float *out, size_t size)
 {
-    float sig, p, c, ad_out, attack, release;
+  float sig, c, ad_out, attack, release, lf, la;
 
     pod.UpdateAnalogControls();
     pod.DebounceControls();
@@ -25,32 +30,48 @@ static void AudioCallback(float *in, float *out, size_t size)
     osc.SetWaveform(wave);
     
     mode += pod.encoder.Increment();
-    mode %= 2;
+    mode %= 3;
     
-    
-    if (mode == 0)
-    {
-        p  = pitch.Process();
-	c = cutoff.Process();
-      	osc.SetFreq(p);
-	flt.SetFreq(c);
-    }
-    else
-    {
-	attack   = pod.GetKnobValue(pod.KNOB_1);
-	release  = pod.GetKnobValue(pod.KNOB_2);
-	ad.SetTime(ADENV_SEG_ATTACK, attack);
-	ad.SetTime(ADENV_SEG_DECAY, release);
-    }
+    switch (mode) {
+      case 0:
+          p  = pitch.Process();
+	  c = cutoff.Process();
+	  flt.SetFreq(c);
+	  pod.led1.Set(0,0,1);
+	  pod.led2.Set(0,0,0);
+	  break;
+      case 1:
+   	  attack   = pod.GetKnobValue(pod.KNOB_1);
+	  release  = pod.GetKnobValue(pod.KNOB_2);
+	  ad.SetTime(ADENV_SEG_ATTACK, attack);
+	  ad.SetTime(ADENV_SEG_DECAY, release);
+	  pod.led1.Set(0,0,0);
+	  pod.led2.Set(0,1,0);
+	  break;
+      case 2:
+  	  lf = lfof.Process();
+	  la = pod.GetKnobValue(pod.KNOB_2);
+	  lfo.SetFreq(lf * 500);
+	  lfo.SetAmp(la * 100);
+	  pod.led1.Set(1,0,0);
+	  pod.led2.Set(1,0,0);
+      default:
+	  break;
+    }	  
 
-    if (pod.button1.RisingEdge())
-      {
-	ad.Trigger();
-      }
+    pod.UpdateLeds();
+    
+    if (pod.button1.RisingEdge() || pod.button2.RisingEdge())
+    {
+  	ad.Trigger();
+    }
     
     for (size_t i = 0; i < size; i += 2)
     {
         ad_out = ad.Process();
+	v = lfo.Process();
+
+	osc.SetFreq(p + v);
 	
 	sig = osc.Process();
 	sig = flt.Process(sig);
@@ -69,29 +90,37 @@ int main(void)
     // initialize pod hardware and oscillator daisysp module
     float sample_rate;
     mode = wave = 0;
+    v = 0.0f;
+    p = 1000.0f;
     
     pod.Init();
     sample_rate = pod.AudioSampleRate();
     osc.Init(sample_rate);
     flt.Init(sample_rate);
     ad.Init(sample_rate);
+    lfo.Init(sample_rate);
     
     // Set parameters for oscillator
     osc.SetWaveform(osc.WAVE_SIN);
     osc.SetFreq(440);
     osc.SetAmp(0.5);
 
+    // Set parameters for lfo
+    lfo.SetWaveform(osc.WAVE_SIN);
+    lfo.SetFreq(0.1);
+    lfo.SetAmp(1);
+    
     //Set envelope parameters
-    ad.SetTime( ADENV_SEG_ATTACK, 0.2);
-    ad.SetTime( ADENV_SEG_DECAY, .01);
+    ad.SetTime( ADENV_SEG_ATTACK, 0.01);
+    ad.SetTime( ADENV_SEG_DECAY, .2);
     ad.SetMax(0.5);
     ad.SetMin(0);
     ad.SetCurve(0.5);
 
     //set parameters for cutoff parameter
-    cutoff.Init(pod.knob1, 0, 20000, cutoff.EXPONENTIAL);
-    pitch.Init(pod.knob2, 0, 20000, pitch.EXPONENTIAL);
-    
+    cutoff.Init(pod.knob1, 0, 10000, cutoff.EXPONENTIAL);
+    pitch.Init(pod.knob2, 0, 10000, pitch.EXPONENTIAL);
+    lfof.Init(pod.knob1, 0, 1000, lfof.LOGARITHMIC);
     
     // start callback
     pod.StartAdc();
