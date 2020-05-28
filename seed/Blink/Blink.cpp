5dc3ba9be312354b1309f5f61d34a2ad281b8348
Stephen Hensley <stephen.p.hensley@gmail.com>
2020-05-28 16:26:31 -0700

    added blink example

---
+++ seed/Blink/Blink.cpp
@@ -0,0 +1,34 @@
+#include "daisy_seed.h"
+
+// Use the daisy namespace to prevent having to type 
+// daisy:: before all libdaisy functions
+using namespace daisy;
+
+// Declare a DaisySeed object called hardware
+DaisySeed hardware;
+
+int main(void)
+{
+    // Declare a variable to store the state we want to set for the LED.
+    bool led_state;
+    led_state = true;
+
+    // Configure and Initialize the Daisy Seed
+    // These are separate to allow reconfiguration of any of the internal
+    // components before initialization.
+    hardware.Configure();
+    hardware.Init();
+
+    // Loop forever
+    for(;;)
+    {
+        // Set the onboard LED
+        hardware.SetLed(led_state);
+
+        // Toggle the LED state for the next time around.
+        led_state = !led_state;
+
+        // Wait 500ms
+        dsy_system_delay(500);
+    }
+}
