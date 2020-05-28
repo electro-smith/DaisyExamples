fd927db883154f9acea712a07de10077c145ffd5
beserge <bensergentanis@gmail.com>
2020-05-28 16:27:05 -0400

    simplified looper

--- pod/Looper/PodLooper.cpp
+++ pod/Looper/PodLooper.cpp
@@ -1,7 +1,13 @@
+// Press button one to record. First recording sets loop length. Automatically starts looping  if 1 second limit is hit.
+// After first loop sound on sound recording begins. Press button one to toggle SOS recording. Hold button one to clear loop.
+// Red lights indicate record enable.
+// Press button 2 to pause/play loop buffer.
+
+
 #include "daisysp.h"
 #include "daisy_pod.h"
 
-const int MAX_SIZE = (int)(48000); //1 mins of floats at 48kHz
+#define MAX_SIZE (48000 * 2) //2 seconds of floats at 48kHz
 
 using namespace daisysp;
 using namespace daisy;
@@ -10,12 +16,12 @@ static DaisyPod pod;
 
 bool first = true; //first loop (sets length)
 bool rec = false; //currently recording
-bool play = false;
-float pos = 0;
+bool play = false; //currently playing
+
+int pos = 0;
 float buf[MAX_SIZE];
 int mod = MAX_SIZE;
 int len = 0;
-int lastpos = 0;
 
 void Reset()
 {
@@ -31,14 +37,10 @@ void Reset()
   len = 0;
 }
 
-static void AudioCallback(float *in, float *out, size_t size)
-{ 
-    float output;
-  
+void Controls()
+{
     pod.UpdateAnalogControls();
     pod.DebounceControls();
-
-    float speed = pod.knob1.Process() * 5 + 1;
     
     if (pod.button1.RisingEdge())
     {
@@ -62,37 +64,50 @@ static void AudioCallback(float *in, float *out, size_t size)
     {
         Reset();
     }
+
+    if (rec)
+    {
+        pod.led1.Set(1,0,0);
+        pod.led2.Set(1,0,0);
+    }
+    else
+    {
+	pod.ClearLeds();
+    }
+    pod.UpdateLeds();
+}
+
+static void AudioCallback(float *in, float *out, size_t size)
+{ 
+    float output;
     
+    Controls();
+   
     for (size_t i = 0; i < size; i += 2)
     {
-        float posrem = pos - (int) pos;
-	int posflr = (int) floor(pos);
-	int posceil = (int) ceil(pos);
-	
-	if (rec)
+       	if (rec)
 	{
-	  buf[(int)floor(pos)] = ((1 - posrem) * buf[posflr] + posrem * buf[posceil]) * 0.5 + in[i] * 0.5;
-	  if (first)
+	    buf[pos] = buf[pos] * 0.5 + in[i] * 0.5;
+	    if (first)
 	    {
 		len++;
 	    }	  
 	}
 
 	
-      	output = (1 - posrem) * buf[(int)floor(pos)] + posrem * buf[(int)ceil(pos)]; //linear interpolation
+      	output = buf[pos];
 	
 	if (len >= MAX_SIZE)
 	{
 	    first = false;
-	    rec   = false;
 	    mod   = MAX_SIZE;
 	    len = 0;
 	}
 
 	if (play)
 	{
-	    pos += speed;
-	    pos = fmod(pos, mod);
+	    pos++;
+	    pos %= mod;
 	}
 	
 	// left out
@@ -108,7 +123,6 @@ int main(void)
     // initialize pod hardware and oscillator daisysp module
     
     pod.Init();
-    //float sample_rate = pod.AudioSampleRate();
     Reset();
     
     // start callback
