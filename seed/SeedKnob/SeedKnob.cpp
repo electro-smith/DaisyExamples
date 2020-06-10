ee734d139c12ce9615b69bb3f3d6f51145084445
beserge <bensergentanis@gmail.com>
2020-06-10 14:26:31 -0400

    fixes #37, comments fritz, sr

--- seed/SeedKnob/SeedKnob.cpp
+++ seed/SeedKnob/SeedKnob.cpp
@@ -8,28 +8,46 @@ using namespace daisy;
 DaisySeed hardware;
 
 int main(void)
-{
-    Led led1;
-    led1.Init(hardware.GetPin(28), false, 10000);
-
-    
-    
+{    
     // Configure and Initialize the Daisy Seed
     // These are separate to allow reconfiguration of any of the internal
     // components before initialization.
     hardware.Configure();
     hardware.Init();
 
+    
+    Led led1;
+    //Initialize led1. We'll plug it into pin 28.
+    //false here indicates the value is uninverted
+    led1.Init(hardware.GetPin(28), false);
+
+    
+    //This is our ADC configuration
     AdcChannelConfig adcConfig;
+    //Configure pin 21 as an ADC input. This is where we'll read the knob.
     adcConfig.InitSingle(hardware.GetPin(21));
+
+    
+    //Initialize the adc with the config we just made
     hardware.adc.Init(&adcConfig, 1);
+    //Start reading values
     hardware.adc.Start();
+
+    
+    //Start the timer, so we can later use the delay
+    dsy_tim_start();
+
     
     // Loop forever
     for(;;)
     {
-        // Set the onboard LED
+        // Set the onboard LED to the value we read from the knob
         led1.Set(hardware.adc.GetFloat(0));
+
+	//Update the led to reflect the set value
 	led1.Update();
+	
+	//wait 1 ms
+	dsy_tim_delay_ms(1);
     }
 }
