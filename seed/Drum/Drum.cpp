f873029a1de743eb801b462f70b1cdc30b6bbb36
beserge <bensergentanis@gmail.com>
2020-06-11 16:06:37 -0400

    comments

--- seed/Drum/Drum.cpp
+++ seed/Drum/Drum.cpp
@@ -19,34 +19,47 @@ Switch kick, snare;
 void AudioCallback(float* in, float* out, size_t size)
 {
     float osc_out, noise_out, snr_env_out, kck_env_out, sig;
+    //Get rid of any bouncing
     snare.Debounce();
     kick.Debounce();
 
+    //If you press the kick button...
     if (kick.RisingEdge())
     {
+        //Trigger both envelopes!
         kickVolEnv.Trigger();
 	kickPitchEnv.Trigger();
     }
 
+    //If press the snare button trigger its envelope
     if (snare.RisingEdge())
     {
         snareEnv.Trigger();
     }
 
+    //Prepare the audio block
     for (size_t i = 0; i < size; i+= 2)
     {
+        //Get the next volume samples
         snr_env_out = snareEnv.Process();
 	kck_env_out = kickVolEnv.Process();
 
+	//Apply the pitch envelope to the kick
 	osc.SetFreq(kickPitchEnv.Process());
+	//Set the kick volume to the envelope's output
 	osc.SetAmp(kck_env_out);
+	//Process the next oscillator sample
 	osc_out = osc.Process();
 
+	//Get the next snare sample
 	noise_out = noise.Process();
+	//Set the sample to the correct volume
 	noise_out *= snr_env_out;
 
+	//Mix the two signals at half volume
 	sig = .5 * noise_out + .5 + osc_out;
-	
+
+	//Set the left and right outputs to the mixed signals
         out[i] = sig;
 	out[i+1] = sig;
     }
@@ -69,30 +82,34 @@ int main(void)
     //Initialize noise
     noise.Init();
     
-    //Initialize envelopes
+    //Initialize envelopes, this one's for the snare amplitude
     snareEnv.Init(samplerate);
     snareEnv.SetTime(ADENV_SEG_ATTACK, .01);
     snareEnv.SetTime(ADENV_SEG_DECAY, .2);
     snareEnv.SetMax(1);
     snareEnv.SetMin(0);
 
-    //Initialize envelopes
+    //This envelope will control the kick oscillator's pitch
+    //Note that this envelope is much faster than the volume
     kickPitchEnv.Init(samplerate);
     kickPitchEnv.SetTime(ADENV_SEG_ATTACK, .01);
     kickPitchEnv.SetTime(ADENV_SEG_DECAY, .05);
     kickPitchEnv.SetMax(400);
     kickPitchEnv.SetMin(50);
 
-    //Initialize envelopes
+    //This one will control the kick's volume
     kickVolEnv.Init(samplerate);
     kickVolEnv.SetTime(ADENV_SEG_ATTACK, .01);
     kickVolEnv.SetTime(ADENV_SEG_DECAY, 1);
     kickVolEnv.SetMax(1);
     kickVolEnv.SetMin(0);
 
+    //Initialize the kick and snare buttons on pins 27 and 28
+    //The callback rate is samplerate / blocksize (48)
     snare.Init(hardware.GetPin(27), samplerate / 48.f);
     kick.Init(hardware.GetPin(28), samplerate / 48.f);
 
+    //Start calling the callback function
     hardware.StartAudio(AudioCallback);
     
     // Loop forever
