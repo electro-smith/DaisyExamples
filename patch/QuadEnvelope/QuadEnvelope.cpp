5117b2ab661d586110cef830081a5684851918c7
beserge <bensergentanis@gmail.com>
2020-06-23 12:23:01 -0400

    envelope time bug

--- patch/QuadEnvelope/QuadEnvelope.cpp
+++ patch/QuadEnvelope/QuadEnvelope.cpp
@@ -4,17 +4,63 @@
 using namespace daisy;
 using namespace daisysp;
 
-
-// Hardware
 DaisyPatch hw;
+AdEnv envelopes[4];
+Parameter envParam[4];
+
+bool lastInput[4];
+int mode;
+
+bool AudioInTrigger(int inIdx, float inVal)
+{
+    bool ret = false;
+    bool currentIn = inVal > .8f;
+
+    if (!lastInput[inIdx] && currentIn)
+    {
+	ret = true;
+    }
+
+    lastInput[inIdx] = currentIn;
+    return ret;
+}
+
+void ProcessControls();
 
 void AudioCallback(float **in, float **out, size_t size)
 {
-    // Synthesis.
-    for(size_t i = 0; i < size; i += 2)
+    ProcessControls();
+    
+    for(size_t i = 0; i < size; i++)
     {
-	out[i] = 0;
-        out[i+1] 0;
+	for (size_t j = 0; j < 4; j++)
+	{
+	    if (AudioInTrigger(j, in[j][i]))
+	    {
+		envelopes[j].Trigger();
+	    }
+	    out[j][i] = envelopes[j].Process();
+	}
+    }
+}
+
+void InitEnvelopes(float samplerate)
+{
+    for (int i = 0; i < 4; i++)
+    {
+	//enveloper values
+	envelopes[i].Init(samplerate);
+	envelopes[i].SetMax(1);
+	envelopes[i].SetMin(0);
+	envelopes[i].SetTime(ADENV_SEG_ATTACK, .1);
+	envelopes[i].SetTime(ADENV_SEG_DECAY, .4);
+	envelopes[i].SetCurve(0);
+
+	//last trigger in
+	lastInput[i] = false;
+	
+	//envelope parameters (att1, dec1, etc.)
+	envParam[i].Init(hw.controls[i], .01, 2, Parameter::EXPONENTIAL);
     }
 }
 
@@ -25,8 +71,33 @@ int main(void)
     hw.Init();
     samplerate = hw.AudioSampleRate();
 
+    InitEnvelopes(samplerate);
+
+    mode = 0;
+    
     // Start the ADC and Audio Peripherals on the Hardware
     hw.StartAdc();
     hw.StartAudio(AudioCallback);
     for(;;) {}
+}
+
+void ProcessEncoder()
+{
+    mode += hw.encoder.Increment();
+    mode = (mode % 2 + 2) % 2;
+}
+
+void ProcessKnobs()
+{
+    for (int i = 0; i < 2; i++)
+    {
+	envelopes[i + 2 * mode].SetTime(ADENV_SEG_ATTACK, envParam[i * 2].Process());
+	envelopes[i + 2 * mode].SetTime(ADENV_SEG_DECAY, envParam[i * 2 + 1].Process());
+    }
+}
+
+void ProcessControls()
+{
+    ProcessEncoder();
+    ProcessKnobs();   
 }
