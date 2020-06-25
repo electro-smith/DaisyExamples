e210a8515885c7e51d854f61e53b4b832b9f287c
beserge <bensergentanis@gmail.com>
2020-06-25 13:03:46 -0400

    simple functionality

--- patch/QuadMixer/QuadMixer.cpp
+++ patch/QuadMixer/QuadMixer.cpp
@@ -8,21 +8,28 @@ DaisyPatch patch;
 
 static void AudioCallback(float **in, float **out, size_t size)
 {
+    float ctrlVal[4];
+    for (int i = 0; i < 4; i++)
+    {
+	ctrlVal[i] = patch.controls[i].Process();
+    }
+    
     for (size_t i = 0; i < size; i += 2)
     {
         for (size_t chn = 0; chn < 4; chn++)
         {
-            out[chn][i] = 0;
+            out[chn][i] = ctrlVal[chn] * in[chn][i];
         }
     }
 }
 
 int main(void)
 {
-    float samplerate;
     patch.Init(); // Initialize hardware (daisy seed, and patch)
-    samplerate = patch.AudioSampleRate();
 
+    patch.display.WriteString("QuadMixer", Font_7x10, true);
+    patch.display.Update();
+    
     patch.StartAdc();
     patch.StartAudio(AudioCallback);
     while(1) {} // loop forever
