bc5c0ddc5c1fb254795677977e6db9bf07fab103
beserge <bensergentanis@gmail.com>
2020-06-26 08:50:58 -0400

    README

--- patch/QuadMixer/QuadMixer.cpp
+++ patch/QuadMixer/QuadMixer.cpp
@@ -11,19 +11,23 @@ static void AudioCallback(float **in, float **out, size_t size)
     float ctrlVal[4];
     for (int i = 0; i < 4; i++)
     {
+	//Get the four control values
 	ctrlVal[i] = patch.controls[i].Process();
     }
     
-    for (size_t i = 0; i < size; i ++)
+    for (size_t i = 0; i < size; i++)
     {
 	float output = 0.f;
+	//sum all four inputs, attenuated by the control levels
 	for (size_t chn = 0; chn < 4; chn++)
         {
-            output += ctrlVal[chn] * in[chn][i];
+	    output += ctrlVal[chn] * in[chn][i];
         }
 
+	//attenuate by 1/4
 	output *= .25f;
-	
+
+	//send the same thing to all 4 outputs
 	for (size_t chn = 0; chn < 4; chn++)
         {
             out[chn][i] = output;
@@ -34,11 +38,15 @@ static void AudioCallback(float **in, float **out, size_t size)
 int main(void)
 {
     patch.Init(); // Initialize hardware (daisy seed, and patch)
-    
+
+    //start audio
     patch.StartAdc();
     patch.StartAudio(AudioCallback);
+
+    
     while(1)
     {
+	//display the four control levels
 	patch.DisplayControls(false);
     }
 }
--- patch/QuadMixer/README.md
+++ patch/QuadMixer/README.md
@@ -1,3 +1,34 @@
 # Description
 
-Simple Quad Audio Mixer
+Simple Four Channel Audio Mixer.
+
+# Controls
+
+| Control | Description | Comment |
+| --- | --- | --- |
+| Ctrls | Sets input level | Each Ctrl goes to the Input of the same number. I.e. Ctrl1 goes with In1, etc. |
+| Ins | Audio Inputs | Audio inputs are normalled In1 > In2 > In3 > In4 |
+| Outs | Audio Outputs | The mixed signal is available on all 4 outputs |
+
+# Code Snippet
+
+```cpp
+for (size_t i = 0; i < size; i++)
+{
+    float output = 0.f;
+    //sum all four inputs, attenuated by the control levels
+    for (size_t chn = 0; chn < 4; chn++)
+    {
+        output += ctrlVal[chn] * in[chn][i];
+    }
+    
+    //attenuate by 1/4
+    output *= .25f;
+    
+    //send the same thing to all 4 outputs
+    for (size_t chn = 0; chn < 4; chn++)
+    {
+        out[chn][i] = output;
+    }
+}
+```
