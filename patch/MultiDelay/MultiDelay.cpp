9c4eca29157d9d7fab26a8834d22ef49acb11bdc
beserge <bensergentanis@gmail.com>
2020-06-26 14:48:28 -0400

    slight crunch when changing times

--- patch/MultiDelay/MultiDelay.cpp
+++ patch/MultiDelay/MultiDelay.cpp
@@ -1,48 +1,140 @@
+//fdbk
+//tap1
+//tap2
+//tap3
+//dry/wet on encoder
+
+// allow for multi ins, but rely on normalling
+
+
 #include "daisysp.h"
 #include "daisy_patch.h"
+#include <string>
+
+#define MAX_DELAY static_cast<size_t>(48000 * 2.5f)
 
 using namespace daisy;
 using namespace daisysp;
 
 DaisyPatch patch;
-Oscillator osc;
-Parameter freqctrl, wavectrl, ampctrl;
+
+DelayLine <float, MAX_DELAY>  DSY_SDRAM_BSS delay[3];
+
+struct delTime{
+    float currentDelay;
+    float delayTarget;
+};
+
+delTime delTimes[3];
+Parameter params[4];
+
+float feedback;
+int drywet;
+
+void ProcessControls();
 
 static void AudioCallback(float **in, float **out, size_t size)
 {
-	float sig, freq, amp;
-	size_t wave;
-    for (size_t i = 0; i < size; i += 2)
+    ProcessControls();
+    
+    for (size_t i = 0; i < size; i ++)
     {
-        // Read Knobs
-        freq = mtof(freqctrl.Process());
-        wave = wavectrl.Process();
-        amp = ampctrl.Process();
-        // Set osc params
-        osc.SetFreq(freq);
-        osc.SetWaveform(wave);
-        osc.SetAmp(amp);
-        //.Process
-    	sig = osc.Process();
-        // Assign Synthesized Waveform to all four outputs.
-        for (size_t chn = 0; chn < 4; chn++)
-        {
-            out[chn][i] = sig;
+	float mix = 0;
+
+	//update delayline with feedback
+	for (int d = 0; d < 3; d++)
+	{
+	    float sig = delay[d].Read();
+	    float toWrite = ((feedback * sig) + in[0][i]);\
+	    delay[d].Write(toWrite);
+	    mix += toWrite;    
+	}
+
+	//apply drywet and attenuate
+	float fdrywet = (float)drywet / 100.f;
+	mix += .25f * ((fdrywet * mix * .3) + ((1.0f - fdrywet) * in[0][i]));
+
+	for (size_t chn = 0; chn < 4; chn++)
+        {   
+	    out[chn][i] = mix;
         }
     }
 }
 
+void InitDelays(float samplerate)
+{
+    for (int i = 0; i < 3; i++)
+    {
+	//Init delays
+	delay[i].Init();
+	//3 delay times
+	params[i].Init(patch.controls[i], samplerate * .05, MAX_DELAY, Parameter::LOGARITHMIC);
+    }
+    
+    //feedback
+    params[3].Init(patch.controls[3], 0, 1, Parameter::LINEAR); 
+}
+
+void UpdateOled();
+
 int main(void)
 {
     float samplerate;
-    int num_waves = Oscillator::WAVE_LAST - 1;
     patch.Init(); // Initialize hardware (daisy seed, and patch)
     samplerate = patch.AudioSampleRate();
-    osc.Init(samplerate); // Init oscillator
-    freqctrl.Init(patch.controls[patch.CTRL_1], 10.0, 110.0f, Parameter::LINEAR);
-    wavectrl.Init(patch.controls[patch.CTRL_2], 0.0, num_waves, Parameter::LINEAR);
-    ampctrl.Init(patch.controls[patch.CTRL_3], 0.0, 0.5f, Parameter::LINEAR);
+    
+    InitDelays(samplerate);
+
+    drywet = 50;
+    
     patch.StartAdc();
     patch.StartAudio(AudioCallback);
-    while(1) {} // loop forever
+    while(1)
+    {
+	UpdateOled();
+    } 
+}
+
+void UpdateOled()
+{
+    patch.display.Fill(false);
+    
+    patch.display.SetCursor(0,0);
+    patch.display.WriteString("Multi Delay", Font_7x10, true);
+
+    patch.display.SetCursor(0, 30);
+    patch.display.WriteString("Dry/Wet:", Font_7x10, true);
+
+    patch.display.SetCursor(60, 30);
+    std::string str = std::to_string(drywet);
+    char* cstr = &str[0];
+    patch.display.WriteString(cstr, Font_7x10, true);
+    
+    patch.display.Update();
+
+}
+
+void ProcessControls()
+{
+    patch.UpdateAnalogControls();
+    patch.DebounceControls();
+
+    //knobs
+    for (int i = 0; i < 3; i++)
+    {
+	delTimes[i].delayTarget = params[i].Process();
+    }
+    feedback = params[3].Process();
+    
+    //encoder
+    drywet += 5 * patch.encoder.Increment();
+    drywet > 100 ? drywet = 100 : drywet = drywet;
+    drywet < 0 ? drywet = 0 : drywet = drywet;
+
+    //set delays
+    for (int i = 0; i < 3; i++)
+    {
+	fonepole(delTimes[i].currentDelay, delTimes[i].delayTarget, .00007f);
+	delay[i].SetDelay(delTimes[i].currentDelay);
+    }
 }
