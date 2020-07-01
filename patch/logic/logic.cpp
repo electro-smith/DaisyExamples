97ae2907f6571442f910620d4f5027dd589c60a3
beserge <bensergentanis@gmail.com>
2020-07-01 10:54:57 -0400

    menu logic and negations

--- patch/logic/logic.cpp
+++ patch/logic/logic.cpp
@@ -33,8 +33,19 @@ struct gate{
 bool inputs[4];
 gate gates[2];
 std::string gateNames[6];
+bool isInverted[6];
 
-bool cvToBool(float in) { return in > .8f; }
+//menu logic
+int menuPos; //6 positions
+bool inSubMenu; //only on gate types
+
+
+bool CvToBool(float in) { return in > .8f; }
+int mod(int x, int m) { return (x % m + m) % m; }
+
+void ProcessControls();
+void ProcessOled();
+void ProcessOutputs();
 
 void InitGateNames()
 {
@@ -46,17 +57,18 @@ void InitGateNames()
     gateNames[5] = " XNOR ";
 }
 
-    void ProcessControls();
-    void ProcessOled();
-    void ProcessOutputs();
-
-
 int main(void)
 {
     patch.Init(); // Initialize hardware (daisy seed, and patch)
 
     InitGateNames();
-    gates[0].gateType = gates[1].gateType = 0; //both init to AND
+
+    //both init to AND
+    gates[0].gateType = gates[1].gateType = 0; 
+    
+    //no inputs are inverted
+    isInverted[0] = isInverted[1] = false;
+    isInverted[2] = isInverted[3] = false;
 
     patch.StartAdc();
     while(1)
@@ -67,13 +79,81 @@ int main(void)
     }
 }
 
+void ProcessIncrement(int increment)
+{
+    //change gate type
+    if (inSubMenu)
+    {
+        if (menuPos == 1){
+            gates[0].gateType += increment;
+            gates[0].gateType = mod(gates[0].gateType, 6);
+        }
+
+        else
+        {
+            gates[1].gateType += increment;
+            gates[1].gateType = mod(gates[1].gateType, 6);
+        }
+    }
+
+    //move through the menu
+    else 
+    {
+        menuPos += increment;
+        menuPos = mod(menuPos, 6);
+    }
+}
+
+void ProcessRisingEdge()
+{   
+    switch (menuPos) 
+    {
+        //flip inversions
+        case 0:
+            isInverted[0] = ! isInverted[0];
+            break;
+        case 2:
+            isInverted[1] = ! isInverted[1];
+            break;
+        case 3:
+            isInverted[2] = ! isInverted[2];
+            break;
+        case 5:
+            isInverted[3] = ! isInverted[3];
+            break;
+        
+        //enter/exit submenu
+        case 1:
+        case 4:
+            inSubMenu = ! inSubMenu;
+    }
+}
+
+void ProcessEncoder()
+{
+    ProcessIncrement(patch.encoder.Increment());
+    
+    if (patch.encoder.RisingEdge())
+    { 
+        ProcessRisingEdge();
+    }
+}
+
 void ProcessControls()
 {
+    patch.UpdateAnalogControls();
+    patch.DebounceControls();
+
     //inputs
     for (int i = 0; i < 4; i++)
     {
-        inputs[i] = cvToBool(patch.controls[i].Process());
+        inputs[i] = CvToBool(patch.controls[i].Process());
+        
+        //invert the input if isInverted says so
+        inputs[i] = isInverted[i] ? ! inputs[i] : inputs[i];
     }
+
+    ProcessEncoder();
 }
 
 void ProcessOutputs()
@@ -82,4 +162,24 @@ void ProcessOutputs()
     dsy_dac_write(DSY_DAC_CHN2, gates[1].Process(inputs[2], inputs[3]) * 4095);
 }
 
-void ProcessOled(){}
+void ProcessOled()
+{
+    patch.display.Fill(false);
+
+    std::string str;
+    char* cstr = &str[0];
+
+    patch.display.SetCursor(0,0);
+    str = "Logic";
+    patch.display.WriteString(cstr, Font_7x10, true);
+
+    patch.display.SetCursor(0,35);
+    str = "1" + gateNames[gates[0].gateType] + "2";
+    patch.display.WriteString(cstr, Font_6x8, true);
+
+    patch.display.SetCursor(65,35);
+    str = "3" + gateNames[gates[1].gateType] + "4";
+    patch.display.WriteString(cstr, Font_6x8, true);
+
+    patch.display.Update();
+}
