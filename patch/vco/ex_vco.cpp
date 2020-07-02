874518bdfd475371556720aac0c15bda61bf0d73
beserge <bensergentanis@gmail.com>
2020-07-02 11:12:28 -0400

    OLED and fine tune control

--- patch/vco/ex_vco.cpp
+++ patch/vco/ex_vco.cpp
@@ -1,21 +1,25 @@
 #include "daisysp.h"
 #include "daisy_patch.h"
+#include <string>
 
 using namespace daisy;
 using namespace daisysp;
 
 DaisyPatch patch;
 Oscillator osc;
-Parameter freqctrl, wavectrl, ampctrl;
+Parameter freqctrl, wavectrl, ampctrl, finectrl;
 
 static void AudioCallback(float **in, float **out, size_t size)
 {
 	float sig, freq, amp;
 	size_t wave;
+
+    patch.UpdateAnalogControls();
+
     for (size_t i = 0; i < size; i += 2)
     {
         // Read Knobs
-        freq = mtof(freqctrl.Process());
+        freq = mtof(freqctrl.Process() + finectrl.Process());
         wave = wavectrl.Process();
         amp = ampctrl.Process();
         // Set osc params
@@ -35,14 +39,28 @@ static void AudioCallback(float **in, float **out, size_t size)
 int main(void)
 {
     float samplerate;
-    int num_waves = Oscillator::WAVE_LAST - 1;
+    int num_waves = Oscillator::WAVE_POLYBLEP_TRI;
     patch.Init(); // Initialize hardware (daisy seed, and patch)
     samplerate = patch.AudioSampleRate();
+   
     osc.Init(samplerate); // Init oscillator
+   
     freqctrl.Init(patch.controls[patch.CTRL_1], 10.0, 110.0f, Parameter::LINEAR);
-    wavectrl.Init(patch.controls[patch.CTRL_2], 0.0, num_waves, Parameter::LINEAR);
-    ampctrl.Init(patch.controls[patch.CTRL_3], 0.0, 0.5f, Parameter::LINEAR);
+    finectrl.Init(patch.controls[patch.CTRL_2], 0.f, 7.f, Parameter::LINEAR);
+    wavectrl.Init(patch.controls[patch.CTRL_3], 0.0, num_waves, Parameter::LINEAR);
+    ampctrl.Init(patch.controls[patch.CTRL_4], 0.0, 0.5f, Parameter::LINEAR);
+
+    //briefly display module name
+    std::string str = "VCO";
+    char* cstr = &str[0];
+    patch.display.WriteString(cstr, Font_7x10, true);
+    patch.display.Update();
+    patch.DelayMs(1000);
+
     patch.StartAdc();
     patch.StartAudio(AudioCallback);
-    while(1) {} // loop forever
+    while(1) 
+    {
+        patch.DisplayControls(false);
+    }
 }
