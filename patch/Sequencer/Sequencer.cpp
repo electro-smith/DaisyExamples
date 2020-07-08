8ee7c909b63619331e7fa6337b750e902dce0b70
beserge <bensergentanis@gmail.com>
2020-07-08 11:54:36 -0400

    needs testing

--- patch/Sequencer/Sequencer.cpp
+++ patch/Sequencer/Sequencer.cpp
@@ -7,13 +7,116 @@ using namespace daisysp;
 
 DaisyPatch patch;
 
+float values[5];
+bool trigs[5];
+int stepNumber;
+bool trigOut;
+
+int menuPos;
+bool inSubMenu;
+
+void UpdateControls();
+void UpdateOled();
+void UpdateOutputs();
+
 int main(void)
 {
     patch.Init(); // Initialize hardware (daisy seed, and patch)
 
+    //init global vars
+    stepNumber = 0;
+    trigOut = false;
+    menuPos = 0;
+    inSubMenu = false;
+
+    for (int i = 0 ; i < 5; i++)
+    {
+        values[i] = 0.f;
+        trigs[i] = false;
+    }
+
     patch.StartAdc();
-    patch.StartAudio(AudioCallback);
     while(1) 
     {
+        UpdateControls();
+        UpdateOled();
+        UpdateOutputs();
     }
+}
+
+void UpdateControls()
+{
+    patch.UpdateAnalogControls();
+    patch.DebounceControls();
+
+    //encoder
+    //can we simplify the menu logic?
+    if (! inSubMenu)
+    {
+        menuPos += patch.encoder.Increment();
+        menuPos = (menuPos % 10 + 10) % 10;
+        
+        if(menuPos < 5)
+        {
+            inSubMenu = patch.encoder.RisingEdge() ? true : false;
+        }
+        else 
+        {
+            trigs[menuPos % 5] = patch.encoder.RisingEdge() ? !trigs[menuPos % 5] : trigs[menuPos % 5];
+        }
+    }
+
+    else
+    {
+        values[menuPos] += .05 * patch.encoder.Increment();
+        values[menuPos] = values[menuPos] < 0.f ? 0.f : values[menuPos];
+        values[menuPos] = values[menuPos] > 5.f ? 5.f : values[menuPos];
+        inSubMenu = patch.encoder.RisingEdge() ? false : true;
+    }
+
+    //gate in
+    if (patch.gate_input[0].Trig() || patch.gate_input[1].Trig())
+    {
+        stepNumber++;
+        stepNumber %= 5;
+        trigOut = trigs[stepNumber];
+    }
+}
+
+void UpdateOled()
+{
+    patch.display.Fill(false);
+
+    patch.display.SetCursor(0,0);
+    std::string str = "Sequencer";
+    char* cstr = &str[0];
+    patch.display.WriteString(cstr, Font_7x10, true);
+
+    //values and trigs
+    for (int i = 0; i < 5; i++)
+    {   
+        sprintf(cstr, "%d", (int)(100.f  * (values[i] / 5)));
+        patch.display.SetCursor(i * 25, 25);
+        patch.display.WriteString(cstr, Font_7x10, true);
+    
+        str = trigs[i % 5] ? "X" : "O";
+        patch.display.SetCursor(i * 25, 45);
+        patch.display.WriteString(cstr, Font_7x10, true);
+    }
+
+    //cursor
+    str = inSubMenu ? "@" : "o";
+    patch.display.SetCursor((menuPos % 5) * 25, (menuPos > 4) * 20 + 15);
+    patch.display.WriteString(cstr, Font_7x10, true);
+
+    patch.display.Update();
+}
+
+void UpdateOutputs()
+{
+    dsy_dac_write(DSY_DAC_CHN1, values[stepNumber] * 819.f);
+    dsy_dac_write(DSY_DAC_CHN2, values[stepNumber] * 819.f);
+
+    dsy_gpio_write(&patch.gate_output, trigOut);
+    trigOut = false;
 }
