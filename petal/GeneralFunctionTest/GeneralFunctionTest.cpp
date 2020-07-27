4036e0a3297a24a2ab41e10ef5b1ff651f538a16
Stephen Hensley <stephen.p.hensley@gmail.com>
2020-07-27 16:23:43 -0700

    updated Pedal Test to latest, and added vscode folder for it.

--- petal/GeneralFunctionTest/GeneralFunctionTest.cpp
+++ petal/GeneralFunctionTest/GeneralFunctionTest.cpp
@@ -2,21 +2,49 @@
 #include <string.h>
 #include "daisy_petal.h"
 #include "daisysp.h"
+#include "fatfs.h"
+
+#define KNOB_ID "KNOB_"
+#define SW_ID "SWITCH_"
+#define ENC_ID "ENCODER_"
+
+#define TEST_FILE_NAME "DaisyTestFile.txt"
+#define TEST_FILE_CONTENTS "This file is used for testing the Daisy breakout boards. Happy Hacking!"
 
 using namespace daisy;
 using namespace daisysp;
 
-void UpdateUsb();
+int  TestSdCard();
+void UpdateUsb(int sd_sta);
 void UpdateLeds();
 
 // Declare a local daisy_petal for hardware access
 DaisyPetal hw;
+// Handler for SD Card Hardware
+SdmmcHandler sd;
+
+// Variable for tracking encoder increments since there is it's not a continuous control like the rest.
+int32_t enc_tracker;
 
 // This runs at a fixed rate to prepare audio samples
 void callback(float *in, float *out, size_t size)
 {
+    int32_t inc;
     hw.DebounceControls();
     hw.UpdateAnalogControls();
+
+    // Handle Enc
+    inc = hw.encoder.Increment();
+    if(inc > 0)
+        enc_tracker += 1;
+    else if(inc < 0)
+        enc_tracker -= 1;
+    // Clip
+    if(enc_tracker > 100)
+        enc_tracker = 100;
+    else if(enc_tracker < 0)
+        enc_tracker = 0;
+
     // Audio Rate Loop
     for(size_t i = 0; i < size; i += 2)
     {
@@ -27,6 +55,7 @@ void callback(float *in, float *out, size_t size)
 
 int main(void)
 {
+    int sdfail;
     uint32_t led_period, usb_period, now;
     uint32_t last_led_update, last_usb_update;
 
@@ -34,12 +63,15 @@ int main(void)
     // USB Freq= 10Hz
     led_period      = 5;
     usb_period      = 100;
-    last_led_update = last_usb_update = now = dsy_system_getnow();
 
     hw.Init();
 
+    last_led_update = last_usb_update = now = dsy_system_getnow();
+
     // Init USB as VCOM for sending Serial Data
     hw.seed.usb_handle.Init(UsbHandle::FS_INTERNAL);
+
+    sdfail = TestSdCard();
 
     hw.StartAdc();
     hw.StartAudio(callback);
@@ -60,18 +92,57 @@ int main(void)
         if(now - last_usb_update > usb_period)
         {
             last_usb_update = now;
-            UpdateUsb();
+            // opposite of sdfail because
+            // sdfail is 1 when fail, 0 when success
+            // we want to display 1 for success in the tester
+            UpdateUsb(!sdfail);
         }
     }
 }
 
-#define KNOB_ID "KNOB_"
-#define SW_ID "SWITCH_"
-#define ENC_ID "ENCODER_"
+// Mounts SD card and reads TEST_FILE
+// returns 0 if successful, else failure.
+int TestSdCard()
+{
+    int      sta;
+    char     inbuff[2048];
+    char     refbuff[2048];
+    char *   buff;
+    uint32_t len, bytesread;
+
+    memset(inbuff, 0, 2048);
+    memset(refbuff, 0, 2048);
+
+    // Init SDMMC
+    sd.Init();
+    dsy_fatfs_init();
+    f_mount(&SDFatFS, SDPath, 1);
+
+    // Fill reference buffer with test contents
+    sprintf(refbuff, "%s", TEST_FILE_CONTENTS);
+    len = strlen(refbuff);
+
+    // Read from file and compare
+    buff = inbuff;
+    if(f_open(&SDFile, TEST_FILE_NAME, FA_READ) == FR_OK)
+    {
+        f_read(&SDFile, buff, len, (UINT *)&bytesread);
+    }
+    if(len == bytesread && strcmp(buff, refbuff) == 0)
+    {
+        sta = 0;
+    }
+    else
+    {
+        sta = 1;
+    }
+    return sta;
+}
+
 // Send Control Data
 // Format:
 // { ctrl_id, ctrl_data }, {ctrl_id, ctrl_data}, etc.
-void UpdateUsb()
+void UpdateUsb(int sd_sta)
 {
     char catbuff[4095];
     char buff[128];
@@ -82,23 +153,28 @@ void UpdateUsb()
     for(size_t i = 0; i < hw.KNOB_LAST; i++)
     {
         sprintf(buff,
-                "{%s%d, %.3f}",
+                "{%s%d, %d},",
                 KNOB_ID,
                 i + 1,
-                hw.GetKnobValue(static_cast<DaisyPetal::Knob>(i)));
+                (int)(hw.GetKnobValue(static_cast<DaisyPetal::Knob>(i))*1000.f));
         strcat(catbuff, buff);
     }
     // Expression
-    sprintf(buff, "{EXP_0, %.3f}", hw.GetExpression());
+    sprintf(buff, "{EXP_0, %d},", (int)(hw.GetExpression() * 1000.f));
     strcat(catbuff, buff);
     // Switches
     for(size_t i = 0; i < hw.SW_LAST; i++)
     {
-        sprintf(buff, "{%s%d, %d}", SW_ID, i + 1, hw.switches[i].Pressed());
+        sprintf(buff, "{%s%d, %d},", SW_ID, i + 1, hw.switches[i].Pressed());
         strcat(catbuff, buff);
     }
     // Enc
-    sprintf(buff, "{ENC_1, %d}", hw.encoder.Pressed());
+    sprintf(buff, "{%s_SW, %d},", ENC_ID, hw.encoder.Pressed());
+    strcat(catbuff, buff);
+    sprintf(buff, "{%s_INC, %ld},", ENC_ID, enc_tracker);
+    strcat(catbuff, buff);
+    // SD Card Status
+    sprintf(buff, "{SDMMC, %d}", sd_sta);
     strcat(catbuff, buff);
     // EndTx
     sprintf(buff, "\r\n");
