9f643bf33e3f6fe1e92650fd8e401fb5d9abd8e8
beserge <bensergentanis@gmail.com>
2020-08-13 17:10:02 -0400

    first attemp

--- petal/FrequencyShifter/FrequencyShifter.cpp
+++ petal/FrequencyShifter/FrequencyShifter.cpp
@@ -4,25 +4,75 @@
 using namespace daisy;
 using namespace daisysp;
 
-DaisyPetal hw;
+DaisyPetal petal;
+PitchShifter ps;
+Oscillator lfo;
+CrossFade fader;
 
-void callback(float **in, float **out, size_t size)
+bool bypassOn;
+
+Parameter lfoFreqParam;
+Parameter shiftTransParam;
+
+void UpdateControls();
+
+void AudioCallback(float **in, float **out, size_t size)
 {
+	UpdateControls();
+	
     for (size_t i = 0; i < size; i++)
     {
-    }
+		float inf  = in[0][i];
+		float process = ps.Process(in[0][i]);
+		out[0][i] = out[1][i] = fader.Process(inf, process); 
+	}
 }
 
 int main(void)
 {
     float samplerate;
+    petal.Init();
+    samplerate = petal.AudioSampleRate();
 
-    hw.Init();
-    samplerate = hw.AudioSampleRate();
+	lfoFreqParam.Init(petal.knob[0], .1, 20, Parameter::LOGARITHMIC);
+	shiftTransParam.Init(petal.knob[2], -12, 12, Parameter::LINEAR);
 
-    hw.StartAdc();
-    hw.StartAudio(callback);
+	lfo.Init(petal.AudioCallbackRate());
+	lfo.SetAmp(1);
+	lfo.SetWaveform(Oscillator::WAVE_SIN);
+
+	ps.Init(samplerate);
+
+	fader.Init();
+
+    petal.StartAdc();
+    petal.StartAudio(AudioCallback);
     while(1) 
     {
-    }
+		petal.SetFootswitchLed((DaisyPetal::FootswitchLed)0, bypassOn);
+		dsy_system_delay(6);
+	}
+}
+
+void UpdateControls()
+{
+	petal.UpdateAnalogControls();
+	petal.DebounceControls();
+
+	//knobs
+	lfo.SetFreq(lfoFreqParam.Process());
+	lfo.SetAmp(petal.knob[1].Process());
+	ps.SetTransposition(shiftTransParam.Process() + (lfo.Process()) * 12);
+	
+	fader.SetPos(petal.knob[3].Process());
+	if (bypassOn)
+	{
+		fader.SetPos(0);
+	}
+	
+	//bypass switch
+	if (petal.switches[0].RisingEdge())
+	{
+		bypassOn = !bypassOn;
+	}
 }
---
+++ petal/FrequencyShifter/README.md
@@ -0,0 +1,14 @@
+## Description
+Frequency shifter. Built in LFO for vibrato type effect.
+
+## Controls
+
+| Control | Description | Comment |
+| --- | --- | --- |
+| Knob 1 | Lfo Frequency | .1 Hz to 20 Hz |
+| Knob 2 | Lfo Amplitude | 0 to 1 octaves bipolar |
+| Knob 3 | Pitchshifter Transposition | -12 to  12 semitones |
+| Knob 4 | Dry/Wet |  |
+| Footswitch 1 | Bypass | |
+| In 1 | Pitchshift In | |
+| Out 1 2 | Pitchshift Out | |
