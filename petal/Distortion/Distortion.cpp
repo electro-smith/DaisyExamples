ae570eae28c7ec113ed2d87123868c0108cce809
beserge <bensergentanis@gmail.com>
2020-08-17 15:15:25 -0400

    bypass, stage gain, stereo, and leds

--- petal/Distortion/Distortion.cpp
+++ petal/Distortion/Distortion.cpp
@@ -6,34 +6,53 @@ using namespace daisy;
 
 static DaisyPetal petal;
 
-float clip(float in)
+float hardClip(float in)
 {
 	in = in > 1.f ? 1.f : in;
 	in = in < -1.f ? -1.f : in;
 	return in;
 }
 
+float softClip(float in)
+{
+	if (in > 0)
+		return 1 - expf(-in);
+	return -1 + expf(in);
+}
+
+bool bypassHard, bypassSoft;
 static void AudioCallback(float **in, float **out, size_t size)
 {
 	petal.UpdateAnalogControls();
 	
-	float gain = petal.knob[0].Process() * 100 + 10;
-	float drywet = petal.knob[1].Process();
+	float softGain = petal.knob[0].Process() * 10 + 1.2;
+	float hardGain = petal.knob[1].Process() * 100 + 1.2;
+	float drywet = petal.knob[2].Process();
+
+	bypassSoft = petal.switches[0].RisingEdge() ? !bypassSoft : bypassSoft;	
+	bypassHard = petal.switches[1].RisingEdge() ? !bypassHard : bypassHard;
 	
     for(size_t i = 0; i < size; i ++)
     {	
-		float wet = in[0][i];
+		for (int chn = 0; chn < 2; chn++)
+		{
+			float wet = in[chn][i];
+			
+			if (!bypassSoft)
+			{
+				wet *= softGain;
+				wet = softClip(wet);
+			}
 		
-		wet *= gain;
-		wet = clip(wet);
-		
-		if (wet > 0)
-			out[0][i] = out[1][i] = 1 - expf(-wet);
-		else
-			out[0][i] = out[1][i] = -1 + expf(wet);
-		
-		out[0][i] = wet * drywet + in[0][i] * (1 - drywet);
-    }
+			if (!bypassHard)
+			{
+				wet *= hardGain;
+				wet = hardClip(wet);
+			}
+			
+			out[chn][i] = wet * drywet + in[chn][i] * (1 - drywet);
+		}
+	}
 
 }
 
@@ -41,10 +60,23 @@ int main(void)
 {
 	petal.Init();
 	
+	bypassHard = bypassSoft = false;
+	
     // start callback
     petal.StartAdc();
     petal.StartAudio(AudioCallback);
     while(1) 
     {
-    }
+		//LED stuff
+		petal.SetFootswitchLed((DaisyPetal::FootswitchLed)0, bypassSoft);
+		petal.SetFootswitchLed((DaisyPetal::FootswitchLed)1, bypassHard);
+		
+		for (int i = 0; i < 8; i++)
+		{
+			petal.SetRingLed((DaisyPetal::RingLed)i, 1.f, 0.f, 0.f);
+		}
+		
+		petal.UpdateLeds();
+		dsy_system_delay(6);
+	}
 }
