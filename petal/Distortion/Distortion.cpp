3f7081f414c9e0280b8834e5c856f806e415db16
U-MSI\Ben <bensergentanis@gmail.com>
2020-10-08 15:13:52 -0400

    forgot to debounce

--- petal/Distortion/Distortion.cpp
+++ petal/Distortion/Distortion.cpp
@@ -24,6 +24,7 @@ bool bypassHard, bypassSoft;
 static void AudioCallback(float **in, float **out, size_t size)
 {
 	petal.UpdateAnalogControls();
+	petal.DebounceControls();
 	
 	float softGain = petal.knob[0].Process() * 10 + 1.2;
 	float hardGain = petal.knob[1].Process() * 100 + 1.2;
