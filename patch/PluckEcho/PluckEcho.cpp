c7e7506b3c7ee0fe044dbf1b9caaa22cb8996281
stephenhensley <stephen.p.hensley@gmail.com>
2020-11-19 10:19:42 -0800

    fixed pluck echo example.

--- patch/PluckEcho/PluckEcho.cpp
+++ patch/PluckEcho/PluckEcho.cpp
@@ -16,34 +16,34 @@ DaisyPatch hw;
 PolyPluck<NUM_VOICES> synth;
 // 10 second delay line on the external SDRAM
 DelayLine<float, MAX_DELAY> DSY_SDRAM_BSS delay;
-ReverbSc  verb;
+ReverbSc                                  verb;
 
 // Persistent filtered Value for smooth delay time changes.
 float smooth_time;
 
 void AudioCallback(float **in, float **out, size_t size)
 {
-    float sig, delsig; // Mono Audio Vars
-    float trig, nn, decay; // Pluck Vars
-    float deltime, delfb, kval; // Delay Vars
+    float sig, delsig;           // Mono Audio Vars
+    float trig, nn, decay;       // Pluck Vars
+    float deltime, delfb, kval;  // Delay Vars
     float dry, send, wetl, wetr; // Effects Vars
     float samplerate;
 
     // Assign Output Buffers
     float *out_left, *out_right;
-    out_left = out[0];
+    out_left  = out[0];
     out_right = out[1];
 
     samplerate = hw.AudioSampleRate();
     hw.DebounceControls();
     hw.UpdateAnalogControls();
 
-	// Handle Triggering the Plucks
+    // Handle Triggering the Plucks
     trig = 0.0f;
     if(hw.encoder.RisingEdge() || hw.gate_input[DaisyPatch::GATE_IN_1].Trig())
         trig = 1.0f;
 
-	// Set MIDI Note for new Pluck notes.
+    // Set MIDI Note for new Pluck notes.
     nn = 24.0f + hw.GetCtrlValue(DaisyPatch::CTRL_1) * 60.0f;
     nn = static_cast<int32_t>(nn); // Quantize to semitones
 
@@ -53,61 +53,61 @@ void AudioCallback(float **in, float **out, size_t size)
 
     // Get Delay Parameters from knobs.
     kval    = hw.GetCtrlValue(DaisyPatch::CTRL_3);
-    deltime = (0.001f + (kval*kval) * 5.0f) * samplerate;
+    deltime = (0.001f + (kval * kval) * 5.0f) * samplerate;
     delfb   = hw.GetCtrlValue(DaisyPatch::CTRL_4);
 
     // Synthesis.
-    for(size_t i = 0; i < size; i += 2)
+    for(size_t i = 0; i < size; i++)
     {
-		// Smooth delaytime, and set.
+        // Smooth delaytime, and set.
         fonepole(smooth_time, deltime, 0.0005f);
         delay.SetDelay(smooth_time);
 
-		// Synthesize Plucks
-        sig        = synth.Process(trig, nn);
+        // Synthesize Plucks
+        sig = synth.Process(trig, nn);
 
-		// Handle Delay
-		delsig     = delay.Read();
+        //		// Handle Delay
+        delsig = delay.Read();
         delay.Write(sig + (delsig * delfb));
 
-		// Create Reverb Send
-        dry        = sig + delsig;
-        send       = dry * 0.6f;
-        verb.Process(send,send, &wetl, &wetr);
+        // Create Reverb Send
+        dry  = sig + delsig;
+        send = dry * 0.6f;
+        verb.Process(send, send, &wetl, &wetr);
 
-		// Output 
-        out_left[i] = dry + wetl;
-        out_right[i]     = dry + wetr;
+        // Output
+        out_left[i]  = dry + wetl;
+        out_right[i] = dry + wetr;
     }
 }
 
 int main(void)
 {
-	// Init everything.
+    // Init everything.
     float samplerate;
     hw.Init();
     samplerate = hw.AudioSampleRate();
-    
+
     //briefly display the module name
-    std::string str = "Pluck Echo";
-    char* cstr = &str[0];
+    std::string str  = "Pluck Echo";
+    char *      cstr = &str[0];
     hw.display.WriteString(cstr, Font_7x10, true);
     hw.display.Update();
     hw.DelayMs(1000);
-    
+
     synth.Init(samplerate);
-    
+
     delay.Init();
     delay.SetDelay(samplerate * 0.8f); // half second delay
-    
+
     verb.Init(samplerate);
     verb.SetFeedback(0.85f);
     verb.SetLpFreq(2000.0f);
-    
+
     // Start the ADC and Audio Peripherals on the Hardware
     hw.StartAdc();
     hw.StartAudio(AudioCallback);
-    for(;;) 
+    for(;;)
     {
         hw.DisplayControls(false);
     }
