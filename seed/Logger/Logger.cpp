1c05e63167759d117646a66689fa7c48ab69389e
Alex Petrov-Savchenko <flanger4@gmail.com>
2020-11-25 04:15:13 +0300

    Added logger profiling

--- seed/Logger/Logger.cpp
+++ seed/Logger/Logger.cpp
@@ -34,6 +34,59 @@ void prepare_messages()
     fillup_msg(test_msg5);
 }
 
+typedef void (PrintFunc)(const char*, ...);
+
+template<PrintFunc function, typename... Va>
+float ProfileFunction(const char* format, Va... va)
+{
+    constexpr int32_t count = 100000; 
+    uint32_t t0 = dsy_tim_get_tick();
+    for (int i = 0; i < count; i++)
+    {
+        function(format, va...);
+    }
+    uint32_t dt = dsy_tim_get_tick() - t0;
+    const float perf = (float)dt / (200 * count); // using secret knowledge about timer frequency...
+    return perf;
+}
+template<LoggerDestination dest, PrintFunc function>
+void ProfilePrint(const char* class_caption, const char* func_caption)
+{
+    const float print_0  = ProfileFunction<function>("");
+    const float print_1  = ProfileFunction<function>("a");
+    const float print_36 = ProfileFunction<function>("abcdefghijklmnopqrstuvwxyz0123456789");
+    const float print_d  = ProfileFunction<function>("%d", 123);
+    const float print_u  = ProfileFunction<function>("%u", 123);
+    const float print_f  = ProfileFunction<function>("%.3f", 123.0f);
+    const float print_F1 = ProfileFunction<function>(FLT_FMT(1), FLT_VAR(1, 123.0f));
+    const float print_F2 = ProfileFunction<function>(FLT_FMT(2), FLT_VAR(2, 123.0f));
+    const float print_F3 = ProfileFunction<function>(FLT_FMT3,   FLT_VAR3(123.0f));
+    const float print_F4 = ProfileFunction<function>(FLT_FMT(4), FLT_VAR(4, 123.0f));
+
+    // linear fit
+    const float slope     = (print_36 - print_1) / (36 - 1);
+    const float intercept = print_1 - slope;
+
+    hw.PrintLine("%s - %s (  empty string):" FLT_FMT3 " us/call", class_caption, func_caption, FLT_VAR3(print_0));
+    hw.PrintLine("%s - %s (   text string):" FLT_FMT3 " us/call +" FLT_FMT3 " us/char", class_caption, func_caption, FLT_VAR3(intercept), FLT_VAR3(slope));
+    hw.PrintLine("%s - %s ( integer value):" FLT_FMT3 " us/call", class_caption, func_caption, FLT_VAR3(print_d));
+    hw.PrintLine("%s - %s (unsigned value):" FLT_FMT3 " us/call", class_caption, func_caption, FLT_VAR3(print_u));
+    hw.PrintLine("%s - %s ( float.3 value):" FLT_FMT3 " us/call", class_caption, func_caption, FLT_VAR3(print_f));
+    hw.PrintLine("%s - %s (FLT_VAR1 value):" FLT_FMT3 " us/call", class_caption, func_caption, FLT_VAR3(print_F1));
+    hw.PrintLine("%s - %s (FLT_VAR2 value):" FLT_FMT3 " us/call", class_caption, func_caption, FLT_VAR3(print_F2));
+    hw.PrintLine("%s - %s (FLT_VAR3 value):" FLT_FMT3 " us/call", class_caption, func_caption, FLT_VAR3(print_F3));
+    hw.PrintLine("%s - %s (FLT_VAR4 value):" FLT_FMT3 " us/call", class_caption, func_caption, FLT_VAR3(print_F4));
+    
+}
+
+template<LoggerDestination dest>
+void ProfileDest(const char* class_caption)
+{
+    Logger<dest>::StartLog(false);
+    ProfilePrint<dest, Logger<dest>::Print>    (class_caption, "Print    ");
+    ProfilePrint<dest, Logger<dest>::PrintLine>(class_caption, "PrintLine");
+}
+
 int main(void)
 {
     // prepare test messages for corner cases
@@ -45,6 +98,13 @@ int main(void)
     hw.Configure();
     hw.Init();
     hw.StartLog(true);  // true == wait for PC: will block until a terminal is connected
+
+    // Profile different destinations
+    // Don't use LOGGER_INTERNAL, since it will be throttled by the connected UART speed
+    hw.PrintLine("Verify CRT floating point format: %.3f", 123.0f); // linker flags modified in the Makefile to enable this
+    ProfileDest<LOGGER_EXTERNAL>("LOGGER_EXTERNAL");
+    ProfileDest<LOGGER_SEMIHOST>("LOGGER_SEMIHOST");
+    ProfileDest<LOGGER_NONE>    ("LOGGER_NONE    ");
 
     // use static method directly
     Logger<LOGGER_INTERNAL>::PrintLine("This may be used anywhere too");
@@ -98,6 +158,61 @@ int main(void)
 
 This should too appear in the log
 Daisy is online
+Verify CRT floating point format: 123.000
+LOGGER_EXTERNAL - Print     (  empty string): 0.404 us/call
+LOGGER_EXTERNAL - Print     (   text string): 0.516 us/call + 0.010 us/char
+LOGGER_EXTERNAL - Print     ( integer value): 1.921 us/call
+LOGGER_EXTERNAL - Print     (unsigned value): 1.905 us/call
+LOGGER_EXTERNAL - Print     ( float.3 value): 4.675 us/call
+LOGGER_EXTERNAL - Print     (FLT_VAR1 value): 4.194 us/call
+LOGGER_EXTERNAL - Print     (FLT_VAR2 value): 4.382 us/call
+LOGGER_EXTERNAL - Print     (FLT_VAR3 value): 4.520 us/call
+LOGGER_EXTERNAL - Print     (FLT_VAR4 value): 4.644 us/call
+LOGGER_EXTERNAL - PrintLine (  empty string): 0.450 us/call
+LOGGER_EXTERNAL - PrintLine (   text string): 0.556 us/call + 0.009 us/char
+LOGGER_EXTERNAL - PrintLine ( integer value): 2.005 us/call
+LOGGER_EXTERNAL - PrintLine (unsigned value): 1.987 us/call
+LOGGER_EXTERNAL - PrintLine ( float.3 value): 4.752 us/call
+LOGGER_EXTERNAL - PrintLine (FLT_VAR1 value): 4.270 us/call
+LOGGER_EXTERNAL - PrintLine (FLT_VAR2 value): 4.487 us/call
+LOGGER_EXTERNAL - PrintLine (FLT_VAR3 value): 4.620 us/call
+LOGGER_EXTERNAL - PrintLine (FLT_VAR4 value): 4.746 us/call
+LOGGER_SEMIHOST - Print     (  empty string): 0.450 us/call
+LOGGER_SEMIHOST - Print     (   text string): 0.598 us/call + 0.016 us/char
+LOGGER_SEMIHOST - Print     ( integer value): 1.880 us/call
+LOGGER_SEMIHOST - Print     (unsigned value): 1.862 us/call
+LOGGER_SEMIHOST - Print     ( float.3 value): 4.745 us/call
+LOGGER_SEMIHOST - Print     (FLT_VAR1 value): 4.256 us/call
+LOGGER_SEMIHOST - Print     (FLT_VAR2 value): 4.406 us/call
+LOGGER_SEMIHOST - Print     (FLT_VAR3 value): 4.543 us/call
+LOGGER_SEMIHOST - Print     (FLT_VAR4 value): 4.676 us/call
+LOGGER_SEMIHOST - PrintLine (  empty string): 0.432 us/call
+LOGGER_SEMIHOST - PrintLine (   text string): 0.607 us/call + 0.016 us/char
+LOGGER_SEMIHOST - PrintLine ( integer value): 2.082 us/call
+LOGGER_SEMIHOST - PrintLine (unsigned value): 2.065 us/call
+LOGGER_SEMIHOST - PrintLine ( float.3 value): 4.858 us/call
+LOGGER_SEMIHOST - PrintLine (FLT_VAR1 value): 4.452 us/call
+LOGGER_SEMIHOST - PrintLine (FLT_VAR2 value): 4.637 us/call
+LOGGER_SEMIHOST - PrintLine (FLT_VAR3 value): 4.772 us/call
+LOGGER_SEMIHOST - PrintLine (FLT_VAR4 value): 4.907 us/call
+LOGGER_NONE     - Print     (  empty string): 0.000 us/call
+LOGGER_NONE     - Print     (   text string): 0.000 us/call + 0.000 us/char
+LOGGER_NONE     - Print     ( integer value): 0.000 us/call
+LOGGER_NONE     - Print     (unsigned value): 0.000 us/call
+LOGGER_NONE     - Print     ( float.3 value): 0.000 us/call
+LOGGER_NONE     - Print     (FLT_VAR1 value): 0.000 us/call
+LOGGER_NONE     - Print     (FLT_VAR2 value): 0.000 us/call
+LOGGER_NONE     - Print     (FLT_VAR3 value): 0.000 us/call
+LOGGER_NONE     - Print     (FLT_VAR4 value): 0.000 us/call
+LOGGER_NONE     - PrintLine (  empty string): 0.000 us/call
+LOGGER_NONE     - PrintLine (   text string): 0.000 us/call + 0.000 us/char
+LOGGER_NONE     - PrintLine ( integer value): 0.000 us/call
+LOGGER_NONE     - PrintLine (unsigned value): 0.000 us/call
+LOGGER_NONE     - PrintLine ( float.3 value): 0.000 us/call
+LOGGER_NONE     - PrintLine (FLT_VAR1 value): 0.000 us/call
+LOGGER_NONE     - PrintLine (FLT_VAR2 value): 0.000 us/call
+LOGGER_NONE     - PrintLine (FLT_VAR3 value): 0.000 us/call
+LOGGER_NONE     - PrintLine (FLT_VAR4 value): 0.000 us/call
 This may be used anywhere too
 Verifying newline character handling:
 1. This should be a single line
@@ -122,22 +237,14 @@ Verify overflow indicators ($$) below
 5. This should be safe for Print() and PrintLine()..............................................................................
 
 Starting timer printout. Verify fractional values
-     0: Elapsed time:  6.561 seconds
-     1: Elapsed time:  7.062 seconds
-     2: Elapsed time:  7.563 seconds
-     3: Elapsed time:  8.064 seconds
-     4: Elapsed time:  8.565 seconds
-     5: Elapsed time:  9.065 seconds
-     6: Elapsed time:  9.567 seconds
-     7: Elapsed time:  10.068 seconds
-     8: Elapsed time:  10.569 seconds
-     9: Elapsed time:  11.070 seconds
-    10: Elapsed time:  11.571 seconds
-    11: Elapsed time:  12.072 seconds
-    12: Elapsed time:  12.573 seconds
-    13: Elapsed time:  13.074 seconds
-    14: Elapsed time:  13.575 seconds
-    15: Elapsed time:  14.076 seconds
-
-
+     0: Elapsed time:  18.226 seconds
+     1: Elapsed time:  18.727 seconds
+     2: Elapsed time:  19.228 seconds
+     3: Elapsed time:  19.729 seconds
+     4: Elapsed time:  20.230 seconds
+     5: Elapsed time:  20.731 seconds
+     6: Elapsed time:  21.232 seconds
+     7: Elapsed time:  0.259 seconds
+     8: Elapsed time:  0.760 seconds
 */
+
