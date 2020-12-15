58d29f5116380c2f7bdec11025c0a5e2c436c35b
stephenhensley <stephen.p.hensley@gmail.com>
2020-12-15 22:34:11 +0000

    Automated: committing clang-format changes

--- patch/Template/Template.cpp
+++ patch/Template/Template.cpp
@@ -14,8 +14,8 @@ void AudioCallback(float **in, float **out, size_t size)
     for(size_t i = 0; i < size; i++)
     {
         out[0][i] = in[0][i];
-        out[1][i] = in[1][i]; 
-        out[2][i] = in[2][i]; 
+        out[1][i] = in[1][i];
+        out[2][i] = in[2][i];
         out[3][i] = in[3][i];
     }
 }
