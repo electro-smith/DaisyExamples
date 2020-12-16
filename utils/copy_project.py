dba7e266744fd507f8375a8c21e3e3bda9db5117
stephenhensley <stephen.p.hensley@gmail.com>
2020-12-16 13:46:14 -0800

    updated script to work with new template hierarchy and to not process ai/png files.

--- utils/copy_project.py
+++ utils/copy_project.py
@@ -4,6 +4,33 @@ import os
 import shutil
 import argparse
 import fileinput
+import codecs
+
+# Opens file at filepath, 
+# creates a temp file where all instances of string "a"
+# are replaced with "b", and then 
+# removes the original file, and renames the temp file.
+def rewrite_replace(filepath, a, b):
+    in_name = os.path.abspath(filepath)
+    in_base = os.path.basename(in_name)
+    t_name = in_name.replace(in_base, 'tmp_{}'.format(in_base))
+    with codecs.open(in_name, 'r', encoding='utf-8') as fi, \
+        codecs.open(t_name, 'w', encoding='utf-8') as fo:
+        for line in fi:
+            newline = line.replace(a, b)
+            fo.write(newline)
+    os.remove(in_name)
+    os.rename(t_name, in_name)
+
+# Takes a filename and a list of extensions
+# returns true if the file name ends with that extension
+def has_extension(fname, ext_list):
+    f, ext = os.path.splitext(fname)
+    print("f: {}\tExtension:{}".format(f, ext))
+    if ext in ext_list:
+        return True
+    else:
+        return False
 
 ## Main Script
 parser = argparse.ArgumentParser(description='copies existing directory containing visual studio project, and replaces instances of the project name within all necessary files. The project filenames and directory name should match.')
@@ -22,15 +49,22 @@ if os.path.isdir(srcAbs):
     # Then make a copy of the folder renaming it to be the same
     shutil.copytree(srcAbs, destAbs)
     # Go through and rename all the files first.
-    found = glob.glob(destAbs+'/*' + srcBase + '*')
+    found = glob.glob(destAbs+ os.path.sep + '*' + os.path.sep + srcBase + '*', recursive=True)
+    found += glob.glob(destAbs+ os.path.sep + '*' + srcBase + '*', recursive=True)
     for f in found:
         s = os.path.abspath(f)
         d = os.path.abspath(f.replace(srcBase, destBase))
         os.rename(s, d)
-    allFiles = glob.glob(destAbs + '/*')
+    allFiles = glob.glob(destAbs + os.path.sep + '*', recursive=True)
+    allFiles += glob.glob(destAbs + os.path.sep + '**' + os.path.sep + '*')
+    # remove unacceptable extensions
+    avoid_exts = ['.ai', '.png']
+    procFiles = list()
     for f in allFiles:
+        if not has_extension(f, avoid_exts):
+            procFiles.append(f)
+    # process files with internal text replacement
+    for f in procFiles:
         if not os.path.isdir(f) and os.path.exists(f):
-            with fileinput.FileInput(f, inplace=True) as modFile:
-                for line in modFile:
-                    print(line.replace(srcBase, destBase), end='')
-
+            rewrite_replace(f, srcBase, destBase)
+    print("done")
