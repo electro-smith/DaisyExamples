347f84a6f80aa778e2ffd47a1765d224c0e2b05b
beserge <bensergentanis@gmail.com>
2020-12-28 15:14:11 -0500

    more harmonicy

--- seed/harmonic/harmonic.cpp
+++ seed/harmonic/harmonic.cpp
@@ -6,13 +6,21 @@ using namespace daisy;
 
 static DaisySeed          seed;
 static HarmonicOscillator harm;
+static Oscillator         lfo;
 
 static void AudioCallback(float *in, float *out, size_t size)
 {
-
-
     for(size_t i = 0; i < size; i += 2)
     {
+        float center = fabsf(lfo.Process()) * 15.f;
+        float amplitudes[16];
+        for(int i = 0; i < 16; i++)
+        {
+            float dist    = fabsf(center - (float)i) + 1.f;
+            amplitudes[i] = 1.f / ((float)dist * 10.f);
+        }
+        harm.SetAmplitudes(amplitudes);
+
         out[i] = out[i + 1] = harm.Process();
     }
 }
@@ -26,16 +34,12 @@ int main(void)
     sample_rate = seed.AudioSampleRate();
 
     harm.Init(sample_rate, 16);
-	harm.SetFreq(10.f);
-	harm.SetFirstHarmIdx(8);
+    harm.SetFreq(55.f);
+    harm.SetFirstHarmIdx(1);
 
-    float amplitudes[16];
-    for(int i = 0; i < 16; i++)
-    {
-        amplitudes[i] = 0.f;
-    }
-    amplitudes[15] = 1.f;
-	harm.SetAmplitudes(amplitudes);
+    lfo.Init(sample_rate);
+    lfo.SetFreq(.05f);
+    lfo.SetAmp(1.f);
 
     // start callback
     seed.StartAudio(AudioCallback);
