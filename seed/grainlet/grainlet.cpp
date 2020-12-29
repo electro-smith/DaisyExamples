214b563c63bf425e8496f8acb022efb56a69e5e7
beserge <bensergentanis@gmail.com>
2020-12-29 16:01:06 -0500

    moved updates to setters

--- seed/grainlet/grainlet.cpp
+++ seed/grainlet/grainlet.cpp
@@ -8,13 +8,14 @@ DaisySeed          hw;
 GrainletOscillator grainlet;
 Oscillator         lfo;
 
-float       f0, f1;
 static void AudioCallback(float *in, float *out, size_t size)
 {
     for(size_t i = 0; i < size; i += 2)
     {
-        float sig = (lfo.Process() + 1.f) * 0.5;
-        out[i] = out[i + 1] = grainlet.Process(f0, f1, sig, 1.f);
+        float sig = fabsf(lfo.Process());
+		grainlet.SetShape(sig);
+		
+        out[i] = out[i + 1] = grainlet.Process();
     }
 }
 
@@ -24,13 +25,14 @@ int main(void)
     hw.Init();
     float sample_rate = hw.AudioSampleRate();
 
-    f0 = 80.f / sample_rate;
-    f1 = 2000.f / sample_rate;
-    grainlet.Init();
+    grainlet.Init(sample_rate);
+	grainlet.SetCarrierFreq(80.f);
+	grainlet.SetFormantFreq(2000.f);
+	grainlet.SetBleed(1.f);
 
     lfo.Init(sample_rate);
     lfo.SetAmp(1.f);
-    lfo.SetFreq(.5f);
+    lfo.SetFreq(.125f);
 
     hw.StartAudio(AudioCallback);
     while(1) {}
