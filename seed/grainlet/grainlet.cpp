49a2c24f474f98e955007d2dafc1a8dfe79d8f7e
beserge <bensergentanis@gmail.com>
2020-12-29 15:39:55 -0500

    fixed StartAudio bug

--- seed/grainlet/grainlet.cpp
+++ seed/grainlet/grainlet.cpp
@@ -4,34 +4,34 @@
 using namespace daisy;
 using namespace daisysp;
 
-DaisySeed hw;
+DaisySeed          hw;
 GrainletOscillator grainlet;
-Oscillator lfo;
+Oscillator         lfo;
 
-float f0, f1;
-void AudioCallback(float *in, float *out, size_t size)
+float       f0, f1;
+static void AudioCallback(float *in, float *out, size_t size)
 {
-	for (size_t i = 0; i < size; i+= 2)
-	{
-		float sig = fabsf(lfo.Process());
-		out[i] = out[i+1] = grainlet.Process(f0, f1, sig, 1.f);
-	}
+    for(size_t i = 0; i < size; i += 2)
+    {
+        float sig = (lfo.Process() + 1.f) * 0.5;
+        out[i] = out[i + 1] = grainlet.Process(f0, f1, sig, 1.f);
+    }
 }
 
 int main(void)
 {
-	hw.Configure();
-	hw.Init();
-	hw.StartAudio(AudioCallback);
-	float sample_rate = hw.AudioSampleRate();
-	
-	f0 = 80.f / sample_rate;
-	f1 = 2000.f / sample_rate;
-	grainlet.Init();
+    hw.Configure();
+    hw.Init();
+    float sample_rate = hw.AudioSampleRate();
 
-	lfo.Init(sample_rate);
-	lfo.SetFreq(.5f);
-	lfo.SetAmp(1.f);
+    f0 = 80.f / sample_rate;
+    f1 = 2000.f / sample_rate;
+    grainlet.Init();
 
-	while(1) {}
+    lfo.Init(sample_rate);
+    lfo.SetAmp(1.f);
+    lfo.SetFreq(.5f);
+
+    hw.StartAudio(AudioCallback);
+    while(1) {}
 }
