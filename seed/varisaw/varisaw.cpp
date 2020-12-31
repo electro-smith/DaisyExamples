b918544d59a08e851f8a6ac7211ef017e9cbf617
beserge <bensergentanis@gmail.com>
2020-12-31 15:59:50 -0500

    working good

--- seed/varisaw/varisaw.cpp
+++ seed/varisaw/varisaw.cpp
@@ -4,21 +4,35 @@
 using namespace daisy;
 using namespace daisysp;
 
-DaisySeed hw;
+DaisySeed             hw;
+VariableSawOscillator varisaw;
+Oscillator            lfo;
+
 void AudioCallback(float **in, float **out, size_t size)
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
-	}
+    for(size_t i = 0; i < size; i++)
+    {
+        float sig = lfo.Process();
+        varisaw.SetPW(sig);
+
+        out[0][i] = out[1][i] = varisaw.Process();
+    }
 }
 
 int main(void)
 {
-	hw.Configure();
-	hw.Init();
-	hw.StartAdc();
-	hw.StartAudio(AudioCallback);
-	while(1) {}
+    hw.Configure();
+    hw.Init();
+    float sample_rate = hw.AudioSampleRate();
+
+    varisaw.Init(sample_rate);
+    varisaw.SetFreq(62.5f);
+    varisaw.SetWaveshape(1.f);
+
+    lfo.Init(sample_rate);
+    lfo.SetAmp(1.f);
+    lfo.SetFreq(1.f);
+
+    hw.StartAudio(AudioCallback);
+    while(1) {}
 }
