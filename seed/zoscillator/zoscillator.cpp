a56f8fc2175dc6736f7ba6f36ed8b86a69f4247a
beserge <bensergentanis@gmail.com>
2020-12-31 15:18:06 -0500

    formatting

--- seed/zoscillator/zoscillator.cpp
+++ seed/zoscillator/zoscillator.cpp
@@ -4,21 +4,43 @@
 using namespace daisy;
 using namespace daisysp;
 
-DaisySeed hw;
+DaisySeed   hw;
+ZOscillator zosc;
+Oscillator  lfo1, lfo2;
+
 void AudioCallback(float **in, float **out, size_t size)
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
-	}
+    for(size_t i = 0; i < size; i++)
+    {
+        float mod1 = lfo1.Process();
+        float mod2 = lfo2.Process();
+
+        //zosc.SetFormantFreq(250.f * (1.f + mod1 * .8f));
+        zosc.SetFormantFreq(0.f);
+        zosc.SetMode(mod2);
+
+        out[0][i] = out[1][i] = zosc.Process();
+    }
 }
 
 int main(void)
 {
-	hw.Configure();
-	hw.Init();
-	hw.StartAdc();
-	hw.StartAudio(AudioCallback);
-	while(1) {}
+    hw.Configure();
+    hw.Init();
+    float sample_rate = hw.AudioSampleRate();
+
+    zosc.Init(sample_rate);
+    zosc.SetCarrierFreq(80.f);
+    zosc.SetShape(1.f);
+
+    lfo1.Init(sample_rate);
+    lfo1.SetAmp(1.f);
+    lfo1.SetFreq(1.f);
+
+    lfo2.Init(sample_rate);
+    lfo2.SetAmp(1.f);
+    lfo2.SetFreq(.5f);
+
+    hw.StartAudio(AudioCallback);
+    while(1) {}
 }
