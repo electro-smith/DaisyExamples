8a3b1be217162f49b52c382a747ed64bf33364b1
beserge <bensergentanis@gmail.com>
2021-01-08 13:24:30 -0500

    samplerate

--- seed/resonator/resonator.cpp
+++ seed/resonator/resonator.cpp
@@ -8,13 +8,11 @@ DaisySeed hw;
 Resonator res;
 Metro tick;
 
-float f;
-
 void AudioCallback(float **in, float **out, size_t size)
 {
 	for (size_t i = 0; i < size; i++)
 	{		
-		float sig = res.Process(f, .5f, .5f, .5f, tick.Process());
+		float sig = res.Process(440.f, .5f, .5f, .5f, tick.Process());
 		out[0][i] = out[1][i] = sig;
 	}
 }
@@ -26,11 +24,9 @@ int main(void)
 	float sample_rate = hw.AudioSampleRate();
 
 	//kMaxNumModes = 24
-	res.Init(.015, 24);
+	res.Init(.015, 24, sample_rate);
 
 	tick.Init(1.f, sample_rate);
-
-	f = 440.f / sample_rate;
 
 	hw.StartAudio(AudioCallback);
 	while(1) {}
