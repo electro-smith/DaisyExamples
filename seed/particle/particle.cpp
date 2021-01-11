298beb55a758666830433ce15e21c77412255c39
beserge <bensergentanis@gmail.com>
2021-01-11 14:49:39 -0500

    broken

--- seed/particle/particle.cpp
+++ seed/particle/particle.cpp
@@ -5,20 +5,37 @@ using namespace daisy;
 using namespace daisysp;
 
 DaisySeed hw;
+Particle particle;
+
 void AudioCallback(float **in, float **out, size_t size)
 {
 	for (size_t i = 0; i < size; i++)
 	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
-	}
+		particle.Sync();
+		out[0][i] = out[1][i] = particle.Process();
+	}	
 }
 
 int main(void)
 {
 	hw.Configure();
 	hw.Init();
-	hw.StartAdc();
+	float sample_rate = hw.AudioSampleRate();
+	
+	particle.Init(sample_rate);
+
+	particle.SetFreq(1000.f);
+
+    particle.SetResonance(1.f);
+
+    particle.SetRandomFreq(1000.f);
+
+    particle.SetDensity(1.f);
+
+    particle.SetGain(1.f);
+
+    particle.SetSpread(1.f);
+	
 	hw.StartAudio(AudioCallback);
 	while(1) {}
 }
