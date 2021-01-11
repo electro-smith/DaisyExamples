fdc7c1e71ac403d8ce8ba5bda9dca517ab5359e4
beserge <bensergentanis@gmail.com>
2021-01-11 15:58:17 +0000

    Automated: committing clang-format changes

--- seed/clockednoise/clockednoise.cpp
+++ seed/clockednoise/clockednoise.cpp
@@ -4,31 +4,31 @@
 using namespace daisy;
 using namespace daisysp;
 
-DaisySeed hw;
+DaisySeed    hw;
 ClockedNoise noise;
-Oscillator lfo;
+Oscillator   lfo;
 
 void AudioCallback(float **in, float **out, size_t size)
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		noise.SetFreq(fabsf(lfo.Process()));
-		out[0][i] = out[1][i] = noise.Process();
-	}
+    for(size_t i = 0; i < size; i++)
+    {
+        noise.SetFreq(fabsf(lfo.Process()));
+        out[0][i] = out[1][i] = noise.Process();
+    }
 }
 
 int main(void)
 {
-	hw.Configure();
-	hw.Init();
-	float sample_rate = hw.AudioSampleRate();
-	
-	noise.Init(sample_rate);
-	
-	lfo.Init(sample_rate);
-	lfo.SetAmp(sample_rate / 3.f);
-	lfo.SetFreq(.125f);
+    hw.Configure();
+    hw.Init();
+    float sample_rate = hw.AudioSampleRate();
 
-	hw.StartAudio(AudioCallback);
-	while(1) {}
+    noise.Init(sample_rate);
+
+    lfo.Init(sample_rate);
+    lfo.SetAmp(sample_rate / 3.f);
+    lfo.SetFreq(.125f);
+
+    hw.StartAudio(AudioCallback);
+    while(1) {}
 }
