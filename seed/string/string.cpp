23aa2af33976717ff7dcb318816ce31e1c75a7f1
beserge <bensergentanis@gmail.com>
2021-01-12 16:40:17 -0500

    adapted from pluck example

--- seed/string/string.cpp
+++ seed/string/string.cpp
@@ -1,24 +1,74 @@
-#include "daisy_seed.h"
 #include "daisysp.h"
+#include "daisy_seed.h"
+#include <algorithm>
 
-using namespace daisy;
+// Shortening long macro for sample rate
+#ifndef sample_rate
+
+#endif
+
+// Interleaved audio definitions
+#define LEFT (i)
+#define RIGHT (i + 1)
+
 using namespace daisysp;
+using namespace daisy;
 
-DaisySeed hw;
-void AudioCallback(float **in, float **out, size_t size)
+static DaisySeed seed;
+
+// Helper Modules
+static Metro tick;
+static String str;
+
+// MIDI note numbers for a major triad
+const float kArpeggio[3] = {48.0f, 52.0f, 55.0f};
+uint8_t     arp_idx;
+
+static void AudioCallback(float *in, float *out, size_t size)
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
-	}
+    float sig_out, freq, trig;
+    for(size_t i = 0; i < size; i += 2)
+    {
+        // When the Metro ticks:
+        // advance the kArpeggio, and trigger the Pluck.
+        trig = 0.0f;
+        if(tick.Process())
+        {
+            freq    = mtof(kArpeggio[arp_idx]); // convert midi nn to frequency.
+            arp_idx = (arp_idx + 1)
+                      % 3; // advance the kArpeggio, wrapping at the end.
+            str.SetFreq(freq);
+            trig = 1.0f;
+        }
+        sig_out = str.Process(trig);
+        // Output
+        out[LEFT]  = sig_out;
+        out[RIGHT] = sig_out;
+    }
 }
 
 int main(void)
 {
-	hw.Configure();
-	hw.Init();
-	hw.StartAdc();
-	hw.StartAudio(AudioCallback);
-	while(1) {}
+    float init_buff[256]; // buffer for Pluck impulse
+
+    // initialize seed hardware and daisysp modules
+    float sample_rate;
+    seed.Configure();
+    seed.Init();
+    sample_rate = seed.AudioSampleRate();
+
+    // Set up Metro to pulse every second
+    tick.Init(1.0f, sample_rate);
+    // Set up Pluck algo
+    str.Init(sample_rate);
+    str.SetDamping(0.9f);
+
+    arp_idx = 0;
+
+
+    // start callback
+    seed.StartAudio(AudioCallback);
+
+
+    while(1) {}
 }
