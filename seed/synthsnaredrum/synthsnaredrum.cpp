f89d729658a104c17a5a25aef96a1de4030cbf14
beserge <bensergentanis@gmail.com>
2021-01-13 13:27:46 -0500

    works no setters

--- seed/synthsnaredrum/synthsnaredrum.cpp
+++ seed/synthsnaredrum/synthsnaredrum.cpp
@@ -5,12 +5,14 @@ using namespace daisy;
 using namespace daisysp;
 
 DaisySeed hw;
+SyntheticSnareDrum sd;
+Metro tick;
+
 void AudioCallback(float **in, float **out, size_t size)
 {
 	for (size_t i = 0; i < size; i++)
 	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
+		out[0][i] = out[1][i] = sd.Process(false, tick.Process(), .2f , .01f, .6f, .6f, .4f);
 	}
 }
 
@@ -18,7 +20,12 @@ int main(void)
 {
 	hw.Configure();
 	hw.Init();
-	hw.StartAdc();
+	float sample_rate = hw.AudioSampleRate();
+
+	tick.Init(2.f, sample_rate);
+	
+	sd.Init(sample_rate);
+
 	hw.StartAudio(AudioCallback);
 	while(1) {}
 }
