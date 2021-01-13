4ffe46110af79b462c80582cda6cfae1eb89624c
beserge <bensergentanis@gmail.com>
2021-01-13 12:27:32 -0500

    working well

--- seed/synthbassdrum/synthbassdrum.cpp
+++ seed/synthbassdrum/synthbassdrum.cpp
@@ -4,28 +4,38 @@
 using namespace daisy;
 using namespace daisysp;
 
-DaisySeed hw;
+DaisySeed         hw;
 SyntheticBassDrum bd;
-Metro tick;
+Metro             tick;
 
 void AudioCallback(float **in, float **out, size_t size)
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		out[0][i] = out[1][i] = bd.Process(false, tick.Process(), .5f, .002f, .5f, .5f, .5f, .5f, .5f);
-	}
+    for(size_t i = 0; i < size; i++)
+    {
+        float t = tick.Process();
+        if(t)
+        {
+            bd.SetAccent(random() / (float)RAND_MAX);
+            bd.SetDirtiness(random() / (float)RAND_MAX);
+            bd.SetDecay(random() / (float)RAND_MAX);
+        }
+        out[0][i] = out[1][i] = bd.Process(t);
+    }
 }
 
 int main(void)
 {
-	hw.Configure();
-	hw.Init();
-	float sample_rate = hw.AudioSampleRate();
-	
-	bd.Init(sample_rate);
-	
-	tick.Init(1.f, sample_rate);
-	
-	hw.StartAudio(AudioCallback);
-	while(1) {}
+    hw.Configure();
+    hw.Init();
+    float sample_rate = hw.AudioSampleRate();
+
+    bd.Init(sample_rate);
+    bd.SetFreq(50.f);
+    bd.SetDirtiness(.5f);
+    bd.SetFmEnvelopeAmount(.6f);
+
+    tick.Init(2.f, sample_rate);
+
+    hw.StartAudio(AudioCallback);
+    while(1) {}
 }
