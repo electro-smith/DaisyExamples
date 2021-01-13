ca8faef0a02987961d8f46e8a4b52f2243e5667f
beserge <bensergentanis@gmail.com>
2021-01-13 11:27:35 -0500

    working, no setters

--- seed/synthbassdrum/synthbassdrum.cpp
+++ seed/synthbassdrum/synthbassdrum.cpp
@@ -5,12 +5,14 @@ using namespace daisy;
 using namespace daisysp;
 
 DaisySeed hw;
+SyntheticBassDrum bd;
+Metro tick;
+
 void AudioCallback(float **in, float **out, size_t size)
 {
 	for (size_t i = 0; i < size; i++)
 	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
+		out[0][i] = out[1][i] = bd.Process(false, tick.Process(), .5f, .002f, .5f, .5f, .5f, .5f, .5f);
 	}
 }
 
@@ -18,7 +20,12 @@ int main(void)
 {
 	hw.Configure();
 	hw.Init();
-	hw.StartAdc();
+	float sample_rate = hw.AudioSampleRate();
+	
+	bd.Init(sample_rate);
+	
+	tick.Init(1.f, sample_rate);
+	
 	hw.StartAudio(AudioCallback);
 	while(1) {}
 }
