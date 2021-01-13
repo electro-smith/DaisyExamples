3231e975c23d8085b4567f6ca10a16aaf19306ab
beserge <bensergentanis@gmail.com>
2021-01-13 16:25:00 -0500

    working

--- seed/hihat/hihat.cpp
+++ seed/hihat/hihat.cpp
@@ -5,26 +5,35 @@ using namespace daisy;
 using namespace daisysp;
 
 DaisySeed hw;
-HiHat<> hihat;
-Metro tick;
+HiHat<>   hihat;
+Metro     tick;
 
 void AudioCallback(float **in, float **out, size_t size)
 {
-	for (size_t i = 0; i < size; i++){
-		out[0][i] = out[1][i] = hihat.Process(false, tick.Process(), .4f, .1f, .5f, .2f, .8f);
-	}
+    for(size_t i = 0; i < size; i++)
+    {
+        bool t = tick.Process();
+        if(t)
+        {
+            hihat.SetDecay(random() / (float)RAND_MAX);
+            hihat.SetSustain((random() / (float)RAND_MAX) > .8f);
+            hihat.SetTone(random() / (float)RAND_MAX);
+            hihat.SetNoisiness(random() / (float)RAND_MAX);
+        }
+        out[0][i] = out[1][i] = hihat.Process(t);
+    }
 }
 
 int main(void)
 {
-	hw.Configure();
-	hw.Init();
-	float sample_rate = hw.AudioSampleRate();
+    hw.Configure();
+    hw.Init();
+    float sample_rate = hw.AudioSampleRate();
 
-	hihat.Init(sample_rate);
-	
-	tick.Init(2.f, sample_rate);	
-	
-	hw.StartAudio(AudioCallback);
-	while(1) {}
+    hihat.Init(sample_rate);
+
+    tick.Init(8.f, sample_rate);
+
+    hw.StartAudio(AudioCallback);
+    while(1) {}
 }
