65276d77a2677de8c5bf59f45a7234bac89967f2
beserge <bensergentanis@gmail.com>
2021-01-14 10:04:14 -0500

    simple

--- seed/analogbassdrum/analogbassdrum.cpp
+++ seed/analogbassdrum/analogbassdrum.cpp
@@ -5,12 +5,14 @@ using namespace daisy;
 using namespace daisysp;
 
 DaisySeed hw;
+AnalogBassDrum bd;
+Metro tick;
+
 void AudioCallback(float **in, float **out, size_t size)
 {
 	for (size_t i = 0; i < size; i++)
 	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
+		out[0][i] = out[1][i] = bd.Process(false, tick.Process(), .1f, .002f, .1f, -.5f, .7f, .8f);
 	}
 }
 
@@ -18,7 +20,11 @@ int main(void)
 {
 	hw.Configure();
 	hw.Init();
-	hw.StartAdc();
+	float sample_rate = hw.AudioSampleRate();
+	
+	bd.Init(sample_rate);
+	tick.Init(1.f, sample_rate);
+	
 	hw.StartAudio(AudioCallback);
 	while(1) {}
 }
