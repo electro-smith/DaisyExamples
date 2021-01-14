020bafdc6778172aeac9246339d3f28ad6bed6bc
beserge <bensergentanis@gmail.com>
2021-01-14 10:39:32 -0500

    good params

--- seed/analogbassdrum/analogbassdrum.cpp
+++ seed/analogbassdrum/analogbassdrum.cpp
@@ -8,11 +8,21 @@ DaisySeed hw;
 AnalogBassDrum bd;
 Metro tick;
 
+float r = 0.f;
+
 void AudioCallback(float **in, float **out, size_t size)
 {
 	for (size_t i = 0; i < size; i++)
 	{
-		out[0][i] = out[1][i] = bd.Process(false, tick.Process(), .1f, .002f, .1f, -.5f, .7f, .8f);
+		bool t = tick.Process();
+		if(t){
+			r = random() / (float)RAND_MAX;
+			r *= 100.f;
+			r -= 50.f;
+		}
+										//sus   trig  acc  f    ton  dec atfm  self_fm
+		out[0][i] = out[1][i] = bd.Process(false, t, .1f, .001f, 1.f, 1.f, 1.f, 20.f);
+		//decay -3 - 0
 	}
 }
 
