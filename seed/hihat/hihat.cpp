ba93009897473d88a68d2231c804b2f4a0ee6b1a
BEN SERGENTANIS <bensergentanis@gmail.com>
2021-01-18 17:55:38 -0500

    Hihat module example (#154)
    * create;
    
    * readme
    
    * working no setters
    
    * working

---
+++ seed/hihat/hihat.cpp
@@ -0,0 +1,39 @@
+#include "daisy_seed.h"
+#include "daisysp.h"
+
+using namespace daisy;
+using namespace daisysp;
+
+DaisySeed hw;
+HiHat<>   hihat;
+Metro     tick;
+
+void AudioCallback(float **in, float **out, size_t size)
+{
+    for(size_t i = 0; i < size; i++)
+    {
+        bool t = tick.Process();
+        if(t)
+        {
+            hihat.SetDecay(random() / (float)RAND_MAX);
+            hihat.SetSustain((random() / (float)RAND_MAX) > .8f);
+            hihat.SetTone(random() / (float)RAND_MAX);
+            hihat.SetNoisiness(random() / (float)RAND_MAX);
+        }
+        out[0][i] = out[1][i] = hihat.Process(t);
+    }
+}
+
+int main(void)
+{
+    hw.Configure();
+    hw.Init();
+    float sample_rate = hw.AudioSampleRate();
+
+    hihat.Init(sample_rate);
+
+    tick.Init(8.f, sample_rate);
+
+    hw.StartAudio(AudioCallback);
+    while(1) {}
+}
