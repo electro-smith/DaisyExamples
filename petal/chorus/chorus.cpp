d01841af060e9b3ee8a787f3378f2de16f73d704
beserge <bensergentanis@gmail.com>
2021-01-20 15:09:33 -0500

    create petal

---
+++ petal/chorus/chorus.cpp
@@ -0,0 +1,24 @@
+#include "daisy_petal.h"
+#include "daisysp.h"
+
+using namespace daisy;
+using namespace daisysp;
+
+DaisyPetal hw;
+void AudioCallback(float **in, float **out, size_t size)
+{
+	hw.ProcessAllControls();
+	for (size_t i = 0; i < size; i++)
+	{
+		out[0][i] = in[0][i];
+		out[1][i] = in[1][i];
+	}
+}
+
+int main(void)
+{
+	hw.Init();
+	hw.StartAdc();
+	hw.StartAudio(AudioCallback);
+	while(1) {}
+}
