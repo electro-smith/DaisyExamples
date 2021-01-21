874df486e93515ae0dacd1aa5c94c737113f2c01
beserge <bensergentanis@gmail.com>
2021-01-21 15:50:35 +0000

    Automated: committing clang-format changes

--- seed/chorus/chorus.cpp
+++ seed/chorus/chorus.cpp
@@ -5,20 +5,20 @@ using namespace daisy;
 using namespace daisysp;
 
 DaisySeed hw;
-void AudioCallback(float **in, float **out, size_t size)
+void      AudioCallback(float **in, float **out, size_t size)
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
-	}
+    for(size_t i = 0; i < size; i++)
+    {
+        out[0][i] = in[0][i];
+        out[1][i] = in[1][i];
+    }
 }
 
 int main(void)
 {
-	hw.Configure();
-	hw.Init();
-	hw.StartAdc();
-	hw.StartAudio(AudioCallback);
-	while(1) {}
+    hw.Configure();
+    hw.Init();
+    hw.StartAdc();
+    hw.StartAudio(AudioCallback);
+    while(1) {}
 }
