6063aa96f97505bbe5bd95da345b7f227df2e8ae
beserge <bensergentanis@gmail.com>
2021-01-29 14:29:12 -0500

    dry/wet

--- petal/flanger/flanger.cpp
+++ petal/flanger/flanger.cpp
@@ -7,7 +7,8 @@ using namespace daisysp;
 Flanger    flanger;
 DaisyPetal hw;
 
-bool effectOn = true;
+bool  effectOn = true;
+float wet;
 
 void AudioCallback(float **in, float **out, size_t size)
 {
@@ -20,9 +21,21 @@ void AudioCallback(float **in, float **out, size_t size)
 
     effectOn ^= hw.switches[0].RisingEdge();
 
+    //encoder
+    wet += hw.encoder.Increment() * .05f;
+    wet = fclamp(wet, 0.f, 1.f);
+
+    wet = hw.encoder.RisingEdge() ? .9f : wet;
+
     for(size_t i = 0; i < size; i++)
     {
-        out[0][i] = out[1][i] = effectOn ? flanger.Process(in[0][i]) : in[0][i];
+        out[0][i] = out[1][i] = in[0][i];
+
+        if(effectOn)
+        {
+            float sig = flanger.Process(in[0][i]);
+            out[0][i] = out[1][i] = sig * wet + in[0][i] * (1.f - wet);
+        }
     }
 }
 
@@ -33,13 +46,29 @@ int main(void)
 
     flanger.Init(sample_rate);
 
+    wet = .9f;
+
     hw.StartAdc();
     hw.StartAudio(AudioCallback);
     while(1)
     {
         hw.DelayMs(6);
+
         hw.ClearLeds();
-        hw.SetFootswitchLed(hw.FOOTSWITCH_LED_1, effectOn);
+        hw.SetFootswitchLed((DaisyPetal::FootswitchLed)0, (float)effectOn);
+
+        int   wet_int  = (int)(wet * 8.f);
+        float wet_frac = wet - wet_int;
+        for(int i = 0; i < wet_int; i++)
+        {
+            hw.SetRingLed((DaisyPetal::RingLed)i, 1.f, 0.f, 0.f);
+        }
+
+        if(wet_int < 8)
+        {
+            hw.SetRingLed((DaisyPetal::RingLed)wet_int, wet_frac, 0.f, 0.f);
+        }
+
         hw.UpdateLeds();
     }
 }
