028c81997eb1a0b2dc4b9522ad7f747ffe0971a8
chrisfromwork <chris@Christophers-MacBook-Pro.local>
2021-02-26 14:38:36 -0800

    add quadraphonic mixer

---
+++ patch/QuadraphonicMixer/rebuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "rebuilding projects:"
+start_dir=$PWD
+HW=hardware_platforms
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "building QuadraphonicMixer"
+cd ${DIR}
+make clean | grep "warning\|error" # grep for silencing make outputs when regenerating everything.
+make | grep "warning\|error"
+cd "$start_dir"
+echo "done"
+
+#!/bin/bash
+read -p "Press any key to exit..." var
