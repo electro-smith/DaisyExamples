608c6ba27b832f8ef8c165d6e6c2f7e0cd341012
beserge <bensergentanis@gmail.com>
2021-04-12 14:19:46 -0400

    Receive transmit example for seed and field
    Should move this to field folder

--- seed/uart/uart.cpp
+++ seed/uart/uart.cpp
@@ -1,44 +1,65 @@
-#include "daisy_seed.h"
+#include "daisy_field.h"
 #include "daisysp.h"
 
 using namespace daisy;
 using namespace daisysp;
 
-DaisySeed hw;
-UartHandler uart;
+#define RX
+//#define TX
 
-void AudioCallback(float **in, float **out, size_t size)
-{
-	for (size_t i = 0; i < size; i++)
-	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
-	}
-}
+DaisyField hw;
+UartHandler uart;
 
 int main(void)
 {
-	hw.Configure();
 	hw.Init();
 
 	UartHandler::Config uart_config;
 	uart_config.baudrate = 31250;
-	uart_config.mode = UartHandler::Config::Mode::MODE_TX_RX;
-	uart_config.parity = UartHandler::Config::Parity::PARITY_EVEN;
+	uart_config.parity = UartHandler::Config::Parity::PARITY_NONE;
 	uart_config.periph = UartHandler::Config::Peripheral::USART_1;
 	uart_config.stopbits = UartHandler::Config::StopBits::STOP_BITS_1;
 	uart_config.pin_config.rx = {DSY_GPIOB, 7};
 	uart_config.pin_config.tx = {DSY_GPIOB, 6};
 
+	#ifdef TX
+		uart_config.mode = UartHandler::Config::Mode::MODE_TX;
+	#else
+		uart_config.mode = UartHandler::Config::Mode::MODE_RX;
+	#endif
+
 	uart.Init(uart_config);
-	uart.StartRx();
 
-	uint8_t buff[] = {0,12,37,49};
+	hw.StartAdc();
 
-	hw.StartAudio(AudioCallback);
 	while(1) {
 		//onboard led on if no errors
-		hw.SetLed(uart.CheckError() == HAL_UART_ERROR_NONE);
-		uart.PollTx(buff, 4);
+		//rx will throw overrun error since we're reading too slow, but that's OK
+		hw.seed.SetLed(uart.CheckError() == HAL_UART_ERROR_NONE);
+
+		#ifdef TX
+			uint8_t buff[] = {0, 11, 27, 13, 104};
+			uart.PollTx(buff, 5);
+		#endif
+
+		#ifdef RX
+			if(!uart.RxActive())
+			{
+				uart.FlushRx();
+				uart.StartRx();
+			}
+
+			else{
+				while(uart.Readable()){
+					hw.display.Fill(false);
+					char cstr[10];
+					sprintf(cstr, "rx: %d", uart.PopRx());
+					hw.display.SetCursor(0,0);
+					hw.display.WriteString(cstr, Font_7x10, true);
+					hw.display.Update();
+					hw.seed.DelayMs(300);
+				}
+			}
+		#endif
 	}
 }
