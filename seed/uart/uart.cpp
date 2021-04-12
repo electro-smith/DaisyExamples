98787a453513320558a8729e01207e11f9f38c46
beserge <bensergentanis@gmail.com>
2021-04-12 15:10:41 -0400

    A bunch of changes
    Rx Tx doesn't care anymore
    removed rx overflow bug
    added counter
    TODO: figure out why parity is broken

--- seed/uart/uart.cpp
+++ seed/uart/uart.cpp
@@ -4,11 +4,9 @@
 using namespace daisy;
 using namespace daisysp;
 
-#define RX
-//#define TX
-
 DaisyField hw;
 UartHandler uart;
+uint8_t counter;
 
 int main(void)
 {
@@ -21,45 +19,39 @@ int main(void)
 	uart_config.stopbits = UartHandler::Config::StopBits::STOP_BITS_1;
 	uart_config.pin_config.rx = {DSY_GPIOB, 7};
 	uart_config.pin_config.tx = {DSY_GPIOB, 6};
-
-	#ifdef TX
-		uart_config.mode = UartHandler::Config::Mode::MODE_TX;
-	#else
-		uart_config.mode = UartHandler::Config::Mode::MODE_RX;
-	#endif
+	uart_config.mode = UartHandler::Config::Mode::MODE_TX_RX;
 
 	uart.Init(uart_config);
 
-	hw.StartAdc();
+	counter = 0;
 
+	hw.StartAdc();
 	while(1) {
 		//onboard led on if no errors
-		//rx will throw overrun error since we're reading too slow, but that's OK
 		hw.seed.SetLed(uart.CheckError() == HAL_UART_ERROR_NONE);
 
-		#ifdef TX
-			uint8_t buff[] = {0, 11, 27, 13, 104};
-			uart.PollTx(buff, 5);
-		#endif
+		//transmit
+		uint8_t buff[] = {counter};
+		uart.PollTx(buff, 1);
+		counter++;
+		hw.seed.DelayMs(300);
 
-		#ifdef RX
-			if(!uart.RxActive())
-			{
-				uart.FlushRx();
-				uart.StartRx();
-			}
+		//receive
+		if(!uart.RxActive())
+		{
+			uart.FlushRx();
+			uart.StartRx();
+		}
 
-			else{
-				while(uart.Readable()){
-					hw.display.Fill(false);
-					char cstr[10];
-					sprintf(cstr, "rx: %d", uart.PopRx());
-					hw.display.SetCursor(0,0);
-					hw.display.WriteString(cstr, Font_7x10, true);
-					hw.display.Update();
-					hw.seed.DelayMs(300);
-				}
+		else{
+			while(uart.Readable()){
+				hw.display.Fill(false);
+				char cstr[10];
+				sprintf(cstr, "rx: %d", uart.PopRx());
+				hw.display.SetCursor(0,0);
+				hw.display.WriteString(cstr, Font_7x10, true);
+				hw.display.Update();
 			}
-		#endif
+		}		
 	}
 }
