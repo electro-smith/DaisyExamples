3704cff08c4bd7385c7dd265faf4eb77e546ec46
beserge <bensergentanis@gmail.com>
2021-04-20 13:14:30 -0400

    usart1, no dma

--- seed/uart/uart.cpp
+++ seed/uart/uart.cpp
@@ -16,9 +16,9 @@ int main(void)
 	uart_config.baudrate = 31250;
 	uart_config.parity = UartHandler::Config::Parity::NONE;
 	uart_config.periph = UartHandler::Config::Peripheral::USART_1;
-	uart_config.stopbits = UartHandler::Config::StopBits::BITS_1;
-	uart_config.pin_config.rx = {DSY_GPIOB, 7};
+	uart_config.stopbits = UartHandler::Config::StopBits::BITS_2;
 	uart_config.pin_config.tx = {DSY_GPIOB, 6};
+	uart_config.pin_config.rx = {DSY_GPIOB, 7};
 	uart_config.mode = UartHandler::Config::Mode::TX_RX;
 	uart_config.wordlength = UartHandler::Config::WordLength::BITS_8;
 
@@ -30,29 +30,27 @@ int main(void)
 	while(1) {
 		//onboard led on if no errors
 		hw.seed.SetLed(uart.CheckError() == HAL_UART_ERROR_NONE);
-
+		
 		//transmit
 		uint8_t buff[] = {counter};
 		uart.PollTx(buff, 1);
 		counter++;
-		hw.seed.DelayMs(300);
+		hw.DelayMs(10);
 
 		//receive
-		if(!uart.RxActive())
-		{
-			uart.FlushRx();
-			uart.StartRx();
-		}
+		uint8_t rxbuff;
+		uart.PollReceive(&rxbuff, 1, 10);
+		uart.FlushRx();
 
-		else{
-			while(uart.Readable()){
-				hw.display.Fill(false);
-				char cstr[10];
-				sprintf(cstr, "rx: %d", uart.PopRx());
-				hw.display.SetCursor(0,0);
-				hw.display.WriteString(cstr, Font_7x10, true);
-				hw.display.Update();
-			}
-		}		
-	}
+		//oled
+		hw.display.Fill(false);				
+
+		char cstr[10];
+		sprintf(cstr, "rx: %d", rxbuff);
+			
+		hw.display.SetCursor(0,0);
+		hw.display.WriteString(cstr, Font_7x10, true);
+			
+		hw.display.Update();
+	}		
 }
