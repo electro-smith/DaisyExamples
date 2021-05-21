ece0d0e6b76d80758e94852d8813b10c6babd538
beserge <bensergentanis@gmail.com>
2021-05-21 11:49:46 -0400

    Logger and prints for 4 message types

--- seed/midi/midi.cpp
+++ seed/midi/midi.cpp
@@ -6,10 +6,48 @@ using namespace daisysp;
 
 DaisySeed hw;
 MidiHandler midi;
+Logger<> logger;
 
 // Typical Switch case for Message Type.
 void HandleMidiMessage(MidiEvent m)
 {
+    switch(m.type)
+    {
+		case NoteOff:
+		{
+			NoteOffEvent p = m.AsNoteOff();
+			logger.Print("Note Off\n");
+			logger.Print("   Note: %d\n", p.note);
+			logger.Print("   Velocity: %d:\n", p.velocity);
+		}
+		break;
+		case NoteOn:
+        {
+            NoteOnEvent p = m.AsNoteOn();
+			logger.Print("Note On\n");
+			logger.Print("   Note: %d\n", p.note);
+			logger.Print("   Velocity: %d:\n", p.velocity);
+        }
+        break;
+		case PolyphonicKeyPressure:
+		{
+			PolyphonicKeyPressureEvent p = m.AsPolyphonicKeyPressureEvent();
+			logger.Print("PKP Event\n");
+			logger.Print("   Note: %d\n", p.note);
+			logger.Print("   Pressure: %d:\n", p.pressure);
+		}
+		break;
+        case ControlChange:
+        {
+            ControlChangeEvent p = m.AsControlChange();
+			logger.Print("Control Change\n");
+			logger.Print("   Control Number: %d\n", p.control_number);
+			logger.Print("   Value: %d:\n", p.value);
+
+        }
+		break;
+        default: break;
+    }
 }
 
 int main(void)
@@ -19,6 +57,7 @@ int main(void)
 
     midi.Init(MidiHandler::INPUT_MODE_UART1, MidiHandler::OUTPUT_MODE_NONE);
     midi.StartReceive();
+	logger.StartLog();
     for(;;)
     {
         midi.Listen();
