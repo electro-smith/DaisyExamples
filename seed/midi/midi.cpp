53c01ed001bde8077def6d5c9fb52dbc46222f1e
beserge <bensergentanis@gmail.com>
2021-05-21 11:36:57 -0400

    create seed midi tester

---
+++ seed/midi/midi.cpp
@@ -0,0 +1,23 @@
+#include "daisy_seed.h"
+#include "daisysp.h"
+
+using namespace daisy;
+using namespace daisysp;
+
+DaisySeed hw;
+void AudioCallback(float **in, float **out, size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		out[0][i] = in[0][i];
+		out[1][i] = in[1][i];
+	}
+}
+
+int main(void)
+{
+	hw.Configure();
+	hw.Init();
+	hw.StartAudio(AudioCallback);
+	while(1) {}
+}
