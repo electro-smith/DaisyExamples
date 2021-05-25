7e8e53bca264f95affd42b26bde295c623362d3c
stephenhensley <stephen.p.hensley@gmail.com>
2021-05-25 16:09:05 -0700

    fixed typenames for audio callback.

--- patch/EnvelopeOscillator/EnvelopeOscillator.cpp
+++ patch/EnvelopeOscillator/EnvelopeOscillator.cpp
@@ -51,7 +51,7 @@ void UpdateControls()
     }
 }
 
-static void AudioCallback(float **in, float **out, size_t size)
+static void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
     UpdateControls();
 
