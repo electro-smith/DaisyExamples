62488ebf81442eb662eebdd4ab4c9eb7d525e2df
beserge <bensergentanis@gmail.com>
2021-05-26 09:58:46 -0400

    Change BAUDRATEPRESCALER_8 to PS_8

--- seed/spi_slave/spi_slave.cpp
+++ seed/spi_slave/spi_slave.cpp
@@ -19,7 +19,7 @@ int main(void)
 	spi_config.clock_polarity = SpiHandle::Config::ClockPolarity::LOW;
 	spi_config.clock_phase = SpiHandle::Config::ClockPhase::ONE_EDGE;
 	spi_config.nss = SpiHandle::Config::NSS::HARD_OUTPUT;
-	spi_config.baud_prescaler = SpiHandle::Config::BaudPrescaler::BAUDRATEPRESCALER_8;
+	spi_config.baud_prescaler = SpiHandle::Config::BaudPrescaler::PS_8;
 	spi_config.datasize = 8;
 	spi_config.mode = SpiHandle::Config::Mode::SLAVE;
 	
