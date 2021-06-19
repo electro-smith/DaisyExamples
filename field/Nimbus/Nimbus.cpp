487815eeb1daada6f6836ee6bcefab10492fbab0
Gregor Noriskin <gregornoriskin@hotmail.com>
2021-06-18 17:24:58 -0700

    Added ability to dynamically map and unmap any or all parameters to any of the remaining 3 CV inputs on the Field

--- field/Nimbus/Nimbus.cpp
+++ field/Nimbus/Nimbus.cpp
@@ -2,8 +2,9 @@
 #include "daisysp.h"
 #include "granular_processor.h"
 
+#define NUM_KNOBS 8
 #define NUM_PARAMS 9
-#define NUM_PAGES 5
+#define NUM_PAGES 8
 
 #define KNOB_TOLERANCE .001f
 
@@ -19,11 +20,18 @@
 #define VALUE_BAR_HEIGHT 10
 #define VALUE_BAR_WIDTH 55
 #define VALUE_BAR_MARGIN 3
+#define MAPPED_PARAM_CONTAINER_WIDTH 18
 
 #define PARAM_BUFFER_SIZE 8
 
 using namespace daisysp;
 using namespace daisy;
+
+enum DEVICE_STATE
+{
+    RUNNING,
+    CV_MAPPING,
+};
 
 enum DISPLAY_PAGE
 {
@@ -32,6 +40,9 @@ enum DISPLAY_PAGE
     PARAMETERS7TO9,
     BUTTONS1,
     BUTTONS2,
+    CVMAPPINGS2,
+    CVMAPPINGS3,
+    CVMAPPINGS4,
 };
 
 enum DISPLAY_ROW
@@ -56,6 +67,9 @@ enum
     LED_PLAYBACK_QUAL_16B_MO        = 1,
     LED_PLAYBACK_QUAL_8B_ST         = 2,
     LED_PLAYBACK_QUAL_8B_MO         = 3,
+    LED_MAP_CV_2                    = 4,
+    LED_MAP_CV_3                    = 5,
+    LED_MAP_CV_4                    = 6,
     LED_SHIFT                       = 7,
     LED_BYPASS                      = 8,
     LED_SILENCE                     = 9,
@@ -79,7 +93,18 @@ enum
     BUTTON_PLAYBACK_QUAL_16B_MO        = 9,
     BUTTON_PLAYBACK_QUAL_8B_ST         = 10,
     BUTTON_PLAYBACK_QUAL_8B_MO         = 11,
+    BUTTON_MAP_CV_2                    = 12,
+    BUTTON_MAP_CV_3                    = 13,
+    BUTTON_MAP_CV_4                    = 14,
     BUTTON_SHIFT                       = 15,
+};
+
+enum MAPPABLE_CVS
+{
+    NONE,
+    CV2,
+    CV3,
+    CV4,
 };
 
 GranularProcessorClouds processor;
@@ -96,55 +121,85 @@ class ParamControl
     ~ParamControl() {}
 
     void Init(const char*    name,
+              const char*    abbreviated_name,
               AnalogControl* knob,
               int            led,
               bool           shifted,
               Parameters*    params,
               int            param_num,
-              DISPLAY_PAGE   display_page)
+              DISPLAY_PAGE   display_page,
+              MAPPABLE_CVS   mapped_cv = NONE)
     {
-        name_         = name;
-        knob_         = knob;
-        led_          = led;
-        shifted_      = shifted;
-        params_       = params;
-        param_num_    = param_num;
-        val_          = Changed(0.f);
-        display_page_ = display_page;
+        display_name_     = name;
+        abbreviated_name_ = abbreviated_name;
+        knob_             = knob;
+        led_              = led;
+        shifted_          = shifted;
+        params_           = params;
+        param_num_        = param_num;
+        param_val_        = ControlChange(0.f);
+        display_page_     = display_page;
+        mapped_cv_        = mapped_cv;
+        knob_val_         = 0.f;
+        knob_val_changed_ = false;
     }
 
-    const char* GetName() { return name_; }
+    bool HasParamChanged() { return param_val_changed_; }
+    bool HasKnobChanged() { return knob_val_changed_; }
 
-    bool Changed(float newval)
+    bool ControlChange(float newval)
     {
-        bool ret = fabsf(newval - val_) > KNOB_TOLERANCE;
-        val_     = newval;
+        bool ret   = fabsf(newval - param_val_) > KNOB_TOLERANCE;
+        param_val_ = newval;
         if(ret)
         {
-            changed_ = true;
-            snprintf(val_str, PARAM_BUFFER_SIZE, "%d", int(val_ * 100));
+            param_val_changed_ = true;
+
+#ifdef SHOW_KNOB_VALUES
+            snprintf(val_str, PARAM_BUFFER_SIZE, "%d", int(param_val_ * 100));
+#endif
         }
         else
         {
-            changed_ = false;
+            param_val_changed_ = false;
         };
         return ret;
     }
 
-    int          GetLed() const { return led_; }
-    float        GetValue() { return val_; }
-    char*        GetValueAsString() { return val_str; }
-    int          GetParamNum() { return param_num_; }
-    bool         GetShifted() { return shifted_; }
-    bool         IsChanged() { return changed_; }
+    const char* GetDisplayName() { return display_name_; }
+    const char* GetAbbreviatedName() { return abbreviated_name_; }
+    int         GetLed() const { return led_; }
+    float       GetValue() { return param_val_; }
+    char*       GetValueAsString() { return val_str; }
+    int         GetParamNum() { return param_num_; }
+    bool        IsShifted() { return shifted_; }
+
     DISPLAY_PAGE GetDisplayPage() { return display_page_; }
+
+    MAPPABLE_CVS GetMappedCV() { return mapped_cv_; }
+    void         SetMappedCV(MAPPABLE_CVS mapped_cv) { mapped_cv_ = mapped_cv; }
 
     void Process()
     {
-        float val = knob_->Process();
-        if(!Changed(val))
+        float val;
+
+        float new_knob_val = knob_->Process();
+        knob_val_changed_  = fabsf(new_knob_val - knob_val_) > KNOB_TOLERANCE;
+        knob_val_          = new_knob_val;
+
+        if(mapped_cv_ == NONE)
         {
-            return;
+            val = new_knob_val;
+
+            if(!ControlChange(val))
+            {
+                return;
+            }
+        }
+        else
+        {
+            //Ignore the knob setting if parameter is mapped to a CV since param_val will be set by the CV
+            val = param_val_;
         }
 
         switch(param_num_)
@@ -165,23 +220,32 @@ class ParamControl
     }
 
   private:
-    const char*    name_;
+    const char*    display_name_;
+    const char*    abbreviated_name_;
     AnalogControl* knob_;
     int            led_;
     bool           shifted_;
     Parameters*    params_;
     int            param_num_;
-    float          val_;
+    float          param_val_;
+    bool           param_val_changed_;
+    float          knob_val_;
+    bool           knob_val_changed_;
     char           val_str[PARAM_BUFFER_SIZE];
-    bool           changed_;
-    DISPLAY_PAGE   display_page_;
+
+    DISPLAY_PAGE display_page_;
+    MAPPABLE_CVS mapped_cv_;
 };
 
 ParamControl param_controls[NUM_PARAMS];
 Parameters*  parameters;
 
+DEVICE_STATE current_device_state = RUNNING;
+MAPPABLE_CVS currently_mapping_cv = NONE;
+bool         can_map[4]           = {true};
+
 DISPLAY_PAGE current_display_page;
-bool         is_silenced, is_bypassed, is_shifted;
+bool         is_silenced, is_bypassed, is_shifted, is_frozen_by_button;
 
 void Controls();
 void UpdateLeds();
@@ -216,13 +280,16 @@ void AudioCallback(AudioHandle::InputBuffer  in,
 void InitParams()
 {
     param_controls[0].Init("Position:",
+                           "Po",
                            &field.knob[0],
+
                            DaisyField::LED_KNOB_1,
                            false,
                            parameters,
                            0,
                            DISPLAY_PAGE::PARAMETERS1TO3);
     param_controls[1].Init("Size:",
+                           "Sz",
                            &field.knob[1],
                            DaisyField::LED_KNOB_2,
                            false,
@@ -230,6 +297,7 @@ void InitParams()
                            1,
                            DISPLAY_PAGE::PARAMETERS1TO3);
     param_controls[2].Init("Pitch:",
+                           "Pi",
                            &field.knob[2],
                            DaisyField::LED_KNOB_3,
                            false,
@@ -237,6 +305,7 @@ void InitParams()
                            2,
                            DISPLAY_PAGE::PARAMETERS1TO3);
     param_controls[3].Init("Density:",
+                           "Dn",
                            &field.knob[3],
                            DaisyField::LED_KNOB_4,
                            false,
@@ -244,6 +313,7 @@ void InitParams()
                            3,
                            DISPLAY_PAGE::PARAMETERS4TO6);
     param_controls[4].Init("Texture:",
+                           "Tx",
                            &field.knob[4],
                            DaisyField::LED_KNOB_5,
                            false,
@@ -251,6 +321,7 @@ void InitParams()
                            4,
                            DISPLAY_PAGE::PARAMETERS4TO6);
     param_controls[5].Init("Dry/Wet:",
+                           "DW",
                            &field.knob[5],
                            DaisyField::LED_KNOB_6,
                            false,
@@ -258,6 +329,7 @@ void InitParams()
                            5,
                            DISPLAY_PAGE::PARAMETERS4TO6);
     param_controls[6].Init("Spread:",
+                           "Sp",
                            &field.knob[6],
                            DaisyField::LED_KNOB_7,
                            false,
@@ -265,13 +337,15 @@ void InitParams()
                            6,
                            DISPLAY_PAGE::PARAMETERS7TO9);
     param_controls[7].Init("Feedback:",
+                           "Fb",
                            &field.knob[7],
                            DaisyField::LED_KNOB_8,
                            false,
                            parameters,
                            7,
                            DISPLAY_PAGE::PARAMETERS7TO9);
-    param_controls[8].Init("Revereb:",
+    param_controls[8].Init("Reverb:",
+                           "Rv",
                            &field.knob[7],
                            DaisyField::LED_KNOB_8,
                            true,
@@ -283,7 +357,7 @@ void InitParams()
 int main(void)
 {
     field.Init();
-    field.SetAudioBlockSize(32); 
+    field.SetAudioBlockSize(32);
     float sample_rate = field.AudioSampleRate();
 
     //init the luts
@@ -302,7 +376,7 @@ int main(void)
 
     InitParams();
 
-    //Process all params once to set intial state
+    //Process all params once to set inital state
     for(int i = 0; i < NUM_PARAMS; i++)
     {
         param_controls[i].Process();
@@ -331,7 +405,7 @@ void RenderParamsPage(DISPLAY_PAGE page)
             field.display.SetCursor(LEFT_BORDER_WIDTH,
                                     (offset + 1) * ROW_HEIGHT);
             field.display.WriteString(
-                param_controls[i].GetName(), DEFAULT_FONT, true);
+                param_controls[i].GetDisplayName(), DEFAULT_FONT, true);
 
             field.display.DrawRect(PARAM_NAME_COL_WIDTH,
                                    (offset + 1) * ROW_HEIGHT,
@@ -357,6 +431,43 @@ void RenderParamsPage(DISPLAY_PAGE page)
                 param_controls[i].GetValueAsString(), Font_6x8, true);
 #endif
             ++offset;
+        }
+    }
+}
+
+void RenderCVMappingsPage(MAPPABLE_CVS cv)
+{
+    if(cv == NONE)
+    {
+        return;
+    }
+
+    int x_offset = 0;
+    int y_offset = 1;
+    field.display.SetCursor(LEFT_BORDER_WIDTH, ROW_HEIGHT);
+    for(int i = 0; i < NUM_PARAMS; i++)
+    {
+        if(param_controls[i].GetMappedCV() == cv)
+        {
+            field.display.SetCursor(
+                LEFT_BORDER_WIDTH + (x_offset * MAPPED_PARAM_CONTAINER_WIDTH)
+                    + 3,
+                (y_offset * ROW_HEIGHT) + 3);
+            field.display.WriteString(
+                param_controls[i].GetAbbreviatedName(), SMALL_FONT, true);
+            field.display.DrawRect(
+                LEFT_BORDER_WIDTH + (x_offset * MAPPED_PARAM_CONTAINER_WIDTH),
+                (y_offset * ROW_HEIGHT),
+                LEFT_BORDER_WIDTH + (x_offset * MAPPED_PARAM_CONTAINER_WIDTH)
+                    + 16,
+                (y_offset * ROW_HEIGHT) + 12,
+                true);
+            x_offset++;
+            if(x_offset * MAPPED_PARAM_CONTAINER_WIDTH > DISPLAY_WIDTH / 2)
+            {
+                x_offset = 0;
+                y_offset++;
+            }
         }
     }
 }
@@ -464,7 +575,6 @@ inline void RenderButtonPage2()
                            is_bypassed);
 }
 
-
 void UpdateOled()
 {
     field.display.Fill(false);
@@ -497,6 +607,21 @@ void UpdateOled()
             field.display.WriteString("Buttons Page 2", DEFAULT_FONT, true);
             RenderButtonPage2();
             break;
+
+        case CVMAPPINGS2:
+            field.display.WriteString("CV2 Mappings", DEFAULT_FONT, true);
+            RenderCVMappingsPage(CV2);
+            break;
+
+        case CVMAPPINGS3:
+            field.display.WriteString("CV3 Mappings", DEFAULT_FONT, true);
+            RenderCVMappingsPage(CV3);
+            break;
+
+        case CVMAPPINGS4:
+            field.display.WriteString("CV4 Mappings", DEFAULT_FONT, true);
+            RenderCVMappingsPage(CV4);
+            break;
         default: break;
     }
 
@@ -507,8 +632,16 @@ void UpdateLeds()
 {
     for(int i = 0; i < 8; i++)
     {
-        field.led_driver.SetLed(param_controls[i].GetLed(),
-                                param_controls[i].GetValue());
+        if(param_controls[i].GetMappedCV() == NONE)
+        {
+            field.led_driver.SetLed(param_controls[i].GetLed(),
+                                    param_controls[i].GetValue());
+        }
+        else
+        {
+            //LED is dark if the parameter is mapped to CV
+            field.led_driver.SetLed(param_controls[i].GetLed(), 0.f);
+        }
     }
 
     auto currentPlaybackMode = processor.playback_mode();
@@ -541,6 +674,22 @@ void UpdateLeds()
         LED_PLAYBACK_QUAL_8B_MO,
         currentPlaybackQuality == PLAYBACK_QUAL_8B_MO ? 1.f : 0.5f);
 
+    field.led_driver.SetLed(LED_MAP_CV_2,
+                            current_device_state == CV_MAPPING
+                                    && currently_mapping_cv == CV2
+                                ? 1.f
+                                : 0.f);
+    field.led_driver.SetLed(LED_MAP_CV_3,
+                            current_device_state == CV_MAPPING
+                                    && currently_mapping_cv == CV3
+                                ? 1.f
+                                : 0.f);
+    field.led_driver.SetLed(LED_MAP_CV_4,
+                            current_device_state == CV_MAPPING
+                                    && currently_mapping_cv == CV4
+                                ? 1.f
+                                : 0.f);
+
     field.led_driver.SetLed(LED_FREEZE, processor.frozen() ? 1.f : 0.5f);
     field.led_driver.SetLed(LED_SILENCE, is_silenced ? 1.f : 0.5f);
     field.led_driver.SetLed(LED_BYPASS, is_bypassed ? 1.f : 0.5f);
@@ -549,31 +698,64 @@ void UpdateLeds()
     field.led_driver.SwapBuffersAndTransmit();
 }
 
-int mod(int n, int m)
+int Mod(int n, int m)
 {
     return ((n % m) + m) % m;
 }
 
+void ProcessParam(ParamControl& pc)
+{
+    pc.Process();
+
+    switch(current_device_state)
+    {
+        case RUNNING:
+            if(pc.HasParamChanged())
+            {
+                //If control is mapped to CV then don't change the page when its value is changed
+                if(pc.GetMappedCV() == NONE)
+                {
+                    current_display_page = pc.GetDisplayPage();
+                }
+            }
+            break;
+
+        case CV_MAPPING:
+            if(can_map[currently_mapping_cv] == true)
+            {
+                //The knob has changed from the last iteration so we are mapping
+                if(pc.HasKnobChanged())
+                {
+                    //If control has not been mapped yet or mapped to another CV then map it to the new CV
+                    if(pc.GetMappedCV() != currently_mapping_cv)
+                    {
+                        pc.SetMappedCV(currently_mapping_cv);
+                        //Only allow one mapping action per key press
+                        can_map[currently_mapping_cv] = false;
+                    }
+                    else
+                    {
+                        //If the control is already mapped and the knob has been moved then unmap it
+                        pc.SetMappedCV(NONE);
+                        //Only allow one mapping action per key press
+                        can_map[currently_mapping_cv] = false;
+                    }
+                }
+            }
+            break;
+
+        default: break;
+    }
+}
+
 void ProcessParams()
 {
-    if(!is_shifted)
+    for(int i = 0; i < NUM_PARAMS; i++)
     {
-        for(int i = 0; i < 8; i++)
+        if((is_shifted && param_controls[i].IsShifted())
+           || (!is_shifted && !param_controls[i].IsShifted()))
         {
-            param_controls[i].Process();
-            if(param_controls[i].IsChanged())
-            {
-                current_display_page = param_controls[i].GetDisplayPage();
-            }
-        }
-    }
-    else
-    {
-        //TODO: Smells funny, but works... sort of
-        param_controls[8].Process();
-        if(param_controls[8].IsChanged())
-        {
-            current_display_page = param_controls[8].GetDisplayPage();
+            ProcessParam(param_controls[i]);
         }
     }
 }
@@ -630,6 +812,7 @@ void ProcessButtons()
 
     if(field.KeyboardRisingEdge(BUTTON_FREEZE))
     {
+        is_frozen_by_button = !is_frozen_by_button;
         processor.ToggleFreeze();
         current_display_page = BUTTONS2;
     }
@@ -657,33 +840,86 @@ void ProcessButtons()
     if(field.sw[0].RisingEdge())
     {
         current_display_page
-            = DISPLAY_PAGE(mod(current_display_page - 1, NUM_PAGES));
+            = DISPLAY_PAGE(Mod(current_display_page - 1, NUM_PAGES));
     }
 
     if(field.sw[1].RisingEdge())
     {
         current_display_page
-            = DISPLAY_PAGE(mod(current_display_page + 1, NUM_PAGES));
+            = DISPLAY_PAGE(Mod(current_display_page + 1, NUM_PAGES));
+    }
+
+    if(field.KeyboardRisingEdge(BUTTON_MAP_CV_2))
+    {
+        current_device_state = CV_MAPPING;
+        currently_mapping_cv = CV2;
+        can_map[CV2]         = true;
+    }
+
+    if(field.KeyboardFallingEdge(BUTTON_MAP_CV_2))
+    {
+        current_device_state = RUNNING;
+        currently_mapping_cv = NONE;
+    }
+
+    if(field.KeyboardRisingEdge(BUTTON_MAP_CV_3))
+    {
+        current_device_state = CV_MAPPING;
+        currently_mapping_cv = CV3;
+        can_map[CV3]         = true;
+    }
+
+    if(field.KeyboardFallingEdge(BUTTON_MAP_CV_3))
+    {
+        current_device_state = RUNNING;
+        currently_mapping_cv = NONE;
+    }
+
+    if(field.KeyboardRisingEdge(BUTTON_MAP_CV_4))
+    {
+        current_device_state = CV_MAPPING;
+        currently_mapping_cv = CV4;
+        can_map[CV4]         = true;
+    }
+
+    if(field.KeyboardFallingEdge(BUTTON_MAP_CV_4))
+    {
+        current_device_state = RUNNING;
+        currently_mapping_cv = NONE;
     }
 
     processor.set_silence(is_silenced);
     processor.set_bypass(is_bypassed);
 }
 
-void ProcessGatesAndTriggers()
+void ProcessGatesTriggersCv()
 {
     //Using CV1 in as a gate for Freeze
     //0.7f should map to 3.5 volts for HIGH state
-    parameters->freeze = field.GetCvValue(field.CV_1) > 0.7f; 
+    if(!is_frozen_by_button)
+    {
+        parameters->freeze = field.GetCvValue(field.CV_1) > 0.7f;
+    }
+
     parameters->trigger = field.gate_in.Trig();
     parameters->gate    = field.gate_in.State();
-}
 
+    //Send CV to all the mapped parameters
+    for(int i = 0; i < NUM_PARAMS; i++)
+    {
+        if(param_controls[i].GetMappedCV() != NONE)
+        {
+            float cv_value = field.GetCvValue(param_controls[i].GetMappedCV());
+            param_controls[i].ControlChange(cv_value);
+            param_controls[i].Process();
+        }
+    }
+}
 
 void Controls()
 {
     field.ProcessAllControls();
     ProcessParams();
+    ProcessGatesTriggersCv();
     ProcessButtons();
-    ProcessGatesAndTriggers();
 }
--- field/Nimbus/README.md
+++ field/Nimbus/README.md
@@ -9,7 +9,7 @@ Originally by Emilie Gillet
 ## Description
 
 Nimbus is a port of Mutable Instrument's Clouds. Clouds is a granular
-audio processor, specializing in making huge clouds of sound from even the tiniest source. Nimbus for Field uses all 8 knobs to control Clouds' parameters. Values of each parameter are shown on the OLED   when the value is changed, or by navigating to the specific parameter page with SW1 and SW2. Nimbus for Field also uses the keys to select the current Playback Mode and Quality, and engage freeze, silence, bypass and shift. Shift is used to allow for the adjustment of additionnal parameters using the knobs. Reverb is currently the only parameter that requires shift to change. Because it shares a knob with the Feedback parameter, it will also affect that parameter when shift is turned off (though it can just be set back to the original value and Reverb will stay where you set it). Given that the Field only has one Gate In, which is used to trigger Clouds, CV1 In is used to trigger freeze (with a voltage of 3.5V or above).
+audio processor, specializing in making huge clouds of sound from even the tiniest source. Nimbus for Field uses all 8 knobs to control Clouds' parameters. Values of each parameter are shown on the OLED   when the value is changed, or by navigating to the specific parameter page with SW1 and SW2. Nimbus for Field also uses the keys to select the current Playback Mode and Quality, and engage freeze, silence, bypass and shift. Shift is used to allow for the adjustment of additional parameters using the knobs. Reverb is currently the only parameter that requires shift to change. Because it shares a knob with the Feedback parameter, it will also affect that parameter when shift is turned off (though it can just be set back to the original value and Reverb will stay where you set it). Given that the Field only has one Gate In, which is used to trigger Clouds, CV1 In is used to trigger freeze (with a voltage of 3.5V or above). This port also allows you to map one or more of the parameters to CV In 2, 3 and 4. You can map as many parameters as you like to each of the CVs inputs, but cannot map a parameter to more than one CV input.
 
 Originally ported by Ben Sergentanis from [pichenettes/eurorack](https://github.com/pichenettes/eurorack)
 
@@ -32,6 +32,9 @@ Originally ported by Ben Sergentanis from [pichenettes/eurorack](https://github.
 | KEY A2 | Select Pitch Shift/Time Stretch Playback Mode | |
 | KEY A3 | Select Looping Delay Playback Mode  | |
 | KEY A4 | Select Spectral Processor Playback Mode  | |
+| KEY A5 | Hold to map and unmap parameter to CV2 | Only one parameter can be mapped or unmapped for each key press |
+| KEY A6 | Hold to map and unmap parameter to CV3 | Only one parameter can be mapped or unmapped for each key press |
+| KEY A7 | Hold to map and unmap parameter to CV4 | Only one parameter can be mapped or unmapped for each key press |
 | KEY A8 | Toggle Shift | Lit when on |
 | KEY B1 | Select 16 bit Stereo Playback Quality | |
 | KEY B2 | Select 16 bit Mono Playback Quality | |
@@ -83,18 +86,30 @@ This page shows the current setting for Playback Mode and Quality.
 
 ### Buttons Page 2
 
-This page shows the current toggeled state of:
+This page shows the current toggled state of:
 
 - shift
 - freeze
 - silence
 - bypass
 
+### CV2 Parameter Mapping Page
+
+This page shows the current parameters that are mapped to CV In 2.
+
+### CV3 Parameter Mapping Page
+
+This page shows the current parameters that are mapped to CV In 3.
+
+### CV4 Parameter Mapping Page
+
+This page shows the current parameters that are mapped to CV In 4.
+
 Refer to the [Clouds Manual](https://mutable-instruments.net/modules/clouds/manual/) for more information on these controls.
 
 #### Freeze
 
-Freeze can be forced on by hitting the B6 key. Freeze will stay on until the key is hit again or if CV1 is connected and a voltage below 3.5v is detected. Unlike Ben's port, CV1 overrides the button setting.
+Freeze can be forced on by hitting the B6 key. Freeze will stay on until the key is hit again or if CV1 is connected and a voltage below 3.5v is detected.
 
 #### Mode
 
@@ -117,3 +132,7 @@ You can also select from four quality / mono stereo modes. These are:
 - 8bit u-law Mono
 
 Refer to the [Clouds Manual](https://mutable-instruments.net/modules/clouds/manual/) section on "Audio Quality".
+
+#### CV In to Parameter Mapping
+
+You can modulate any or all of the 9 Nimbus parameters with CV In 2, 3, and 4 (1 is reserved for Freeze). To map a parameter just hold down the key associated with the CV  you wish to map and then move the knob associated with the parameter. The CV mapping keys are not lit but will be when you hold them down. Once you have finished mapping a parameter, release the key. You can only map or unmap  one parameter for each hold of the key. To unmap a parameter just press the key again and move the knob. When a parameter is mapped, input from the knob is ignored and the LED will be dark. The display will still show changes in the parameter values though. You can see which parameters are currently mapped by navigating to each of the CV mapping pages.
