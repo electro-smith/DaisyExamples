f974019783b395fb8d87bad54731a700e978fd95
Stephen Hensley <shensley.Audio@gmail.com>
2021-09-09 11:00:54 -0700

    CI Improvements - Proper Failure, and faster builds (#217)
    * added ability to fail to builds properly, and improved speed of CI
    
    * filtering out 'experimental' folders.
    
    * removed effectively empty Patch3Test directory.
    
    * fixed case issue in new patch_sm exmaples to pass CI.
    
    Co-authored-by: stephenhensley <stephen.p.hensley@gmail.com>

--- ci/build_examples.sh
+++ ci/build_examples.sh
@@ -2,13 +2,27 @@
 echo "building examples:"
 start_dir=$PWD
 example_dirs=( seed pod patch petal field versio patch_sm )
+
+# Loop through the folders within each example dir
+# ignoring folders containing the name, "experiemental"
+# stop on a fail
 for e in ${example_dirs[@]}; do
     for d in $e/*/; do
-        echo "building $d"
-        cd "$d"
-        make clean | grep "warning\|error" # grep for silencing make outputs when regenerating everything.
-        make | grep "warning\|error"
-        cd "$start_dir"
-        echo "done"
+        case $d in
+            *"experimental"*)
+                ;;
+            *)
+                echo "building $d"
+                cd $d
+                make -s clean;
+                make -s
+                if [ $? -ne 0 ]; then
+                    echo "Failed to compile $d"
+                    exit 1
+                fi
+                cd "$start_dir"
+                echo "done"
+        esac
+
     done
 done
