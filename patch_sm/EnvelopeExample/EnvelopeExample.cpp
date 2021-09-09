34ff153a7023af42f7a254a2b046d24b62d163b5
stephenhensley <stephen.p.hensley@gmail.com>
2021-09-09 16:05:45 -0700

    moved the complicated DAC bits to the board support.

--- patch_sm/EnvelopeExample/EnvelopeExample.cpp
+++ patch_sm/EnvelopeExample/EnvelopeExample.cpp
@@ -10,10 +10,7 @@ using namespace daisysp;
 
 /** Our hardware board class handles the interface to the actual DaisyPatchSM
  * hardware. */
-DaisyPatchSM hw;
-
-/** Create a buffer for the CV output */
-uint16_t DMA_BUFFER_MEM_SECTION cv_output_buffer[16];
+DaisyPatchSM patch;
 
 /** Create ADSR envelope module */
 Adsr envelope;
@@ -28,7 +25,7 @@ Switch button;
 void EnvelopeCallback(uint16_t **output, size_t size)
 {
     /** Process the controls */
-    hw.ProcessAnalogControls();
+    patch.ProcessAnalogControls();
     button.Debounce();
 
     /** Set the input value of the ADSR */
@@ -43,47 +40,37 @@ void EnvelopeCallback(uint16_t **output, size_t size)
     }
 
     /** Assign four controls to the times and level of the envelope segments */
-    envelope.SetTime(ADSR_SEG_ATTACK, 0.01 + (hw.GetAdcValue(0)));
-    envelope.SetTime(ADSR_SEG_DECAY, 0.01 + (hw.GetAdcValue(1)));
-    envelope.SetSustainLevel(hw.GetAdcValue(2));
-    envelope.SetTime(ADSR_SEG_RELEASE, 0.01 + (hw.GetAdcValue(3)));
+    envelope.SetTime(ADSR_SEG_ATTACK, 0.01 + (patch.GetAdcValue(0)));
+    envelope.SetTime(ADSR_SEG_DECAY, 0.01 + (patch.GetAdcValue(1)));
+    envelope.SetSustainLevel(patch.GetAdcValue(2));
+    envelope.SetTime(ADSR_SEG_RELEASE, 0.01 + (patch.GetAdcValue(3)));
 
     /** Loop through the samples, and output the ADSR signal */
     for(size_t i = 0; i < size; i++)
     {
         uint16_t value = (envelope.Process(env_state) * 4095.0);
         output[0][i]   = value;
+        output[1][i]   = value;
     }
 }
 
 int main(void)
 {
     /** Initialize the hardware */
-    hw.Init();
+    patch.Init();
 
     /** Initialize the gate input to pin B10 (Gate 1 on the MicroPatch Eval board) */
-    dsy_gpio_pin gate_pin = hw.GetPin(B10);
+    dsy_gpio_pin gate_pin = patch.GetPin(B10);
     gate.Init(&gate_pin);
 
     /** Initialize the button input to pin B7 (Button on the MicroPatch Eval board) */
-    button.Init(hw.GetPin(B7), 48000 / 16);
+    button.Init(patch.GetPin(B7), 48000 / 16);
 
     /** Initialize the ADSR */
     envelope.Init(48000);
 
     /** Configure the DAC to generate audio-rate signals in a callback */
-    DacHandle::Config dac_config;
-    dac_config.mode = DacHandle::Mode::DMA;
-    dac_config.bitdepth
-        = DacHandle::BitDepth::BITS_12; /**< Sets the output value to 0-4095 */
-    dac_config.chn               = DacHandle::Channel::ONE; /** CV Output 1 */
-    dac_config.buff_state        = DacHandle::BufferState::ENABLED;
-    dac_config.target_samplerate = 48000;
-
-    DacHandle dac;
-    dac.Init(dac_config);
-
-    dac.Start(cv_output_buffer, 16, EnvelopeCallback);
+    patch.StartDac(EnvelopeCallback);
 
     while(1) {}
 }
