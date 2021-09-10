bf78769130370fcf7d169f608169409db3a181e0
stephenhensley <stephen.p.hensley@gmail.com>
2021-09-10 12:10:41 -0700

    cleaned up Random CV example

--- patch_sm/RandomCvExample/RandomCvExample.cpp
+++ patch_sm/RandomCvExample/RandomCvExample.cpp
@@ -6,24 +6,15 @@ DaisyPatchSM patch;
 
 int main(void)
 {
+    /** Initialize the hardware */
     patch.Init();
-
-    /** If we want to keep this, we can move this into the System::Init.. */
-    Random::Init();
-
-    float output_voltage = 0.f;
     while(1)
     {
         /** Delay half a second */
-        System::Delay(500);
-
-        /** C-Style - Generate a random voltage between 0.0 and 5.0 */
-        //output_voltage = rand() / (RAND_MAX / 5);
-
-        /** OR use the internal TRNG */
-        output_voltage = Random::GetFloat(0.0, 5.0);
-
-        /** channel 0 means both CV Outputs */
-        patch.WriteCvOut(0, output_voltage);
+        patch.Delay(500);
+        /** Get a truly random float from the hardware */
+        float voltage = patch.GetRandomFloat(0.0, 5.0);
+        /** Write it to both CV Outputs */
+        patch.WriteCvOut(patch.CV_OUT_BOTH, voltage);
     }
 }
