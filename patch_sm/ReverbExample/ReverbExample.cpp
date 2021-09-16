daa24d512b6d32ec6a747fde1989c9970220e88a
stephenhensley <stephen.p.hensley@gmail.com>
2021-09-15 17:25:06 -0700

    new version with requested mappings, cv pin names.

--- patch_sm/ReverbExample/ReverbExample.cpp
+++ patch_sm/ReverbExample/ReverbExample.cpp
@@ -3,15 +3,10 @@
 
 using namespace daisy;
 using namespace daisysp;
+using namespace patch_sm;
 
 DaisyPatchSM patch;
 ReverbSc     reverb;
-
-MappedFloatValue param_time(0.3f, 0.99f, 0.85f);
-MappedFloatValue
-                 param_damp(1000.f, 19000.f, 12000.f, MappedFloatValue::Mapping::log);
-MappedFloatValue param_in_level(0.0f, 1.f, 0.7f);
-MappedFloatValue param_send_level(0.3f, 0.99f, 0.7f);
 
 void AudioCallback(AudioHandle::InputBuffer  in,
                    AudioHandle::OutputBuffer out,
@@ -20,20 +15,21 @@ void AudioCallback(AudioHandle::InputBuffer  in,
     patch.ProcessAnalogControls();
 
     /** Update Params with the four knobs */
-    param_time.SetFrom0to1(patch.GetAdcValue(patch.CV_1));
-    param_damp.SetFrom0to1(patch.GetAdcValue(patch.CV_2));
-    param_in_level.SetFrom0to1(patch.GetAdcValue(patch.CV_3));
-    param_send_level.SetFrom0to1(patch.GetAdcValue(patch.CV_4));
+    float time = fmap(patch.GetAdcValue(CV_1), 0.3f, 0.99f);
+    float damp = fmap(patch.GetAdcValue(CV_2), 1000.f, 19000.f, Mapping::LOG);
+    float in_level   = patch.GetAdcValue(CV_3);
+    float send_level = patch.GetAdcValue(CV_3);
 
-    reverb.SetFeedback(param_time);
-    reverb.SetLpFreq(param_damp);
+
+    reverb.SetFeedback(time);
+    reverb.SetLpFreq(damp);
 
     for(size_t i = 0; i < size; i++)
     {
-        float dryl  = IN_L[i] * param_in_level;
-        float dryr  = IN_R[i] * param_in_level;
-        float sendl = IN_L[i] * param_send_level;
-        float sendr = IN_R[i] * param_send_level;
+        float dryl  = IN_L[i] * in_level;
+        float dryr  = IN_R[i] * in_level;
+        float sendl = IN_L[i] * send_level;
+        float sendr = IN_R[i] * send_level;
         float wetl, wetr;
         reverb.Process(sendl, sendr, &wetl, &wetr);
         OUT_L[i] = dryl + wetl;
@@ -47,22 +43,8 @@ int main(void)
     reverb.Init(patch.AudioSampleRate());
 
     patch.StartAudio(AudioCallback);
-    bool  ledstate = true;
-    float phs      = 0.f;
-    patch.WriteCvOut(patch.CV_OUT_BOTH, ledstate ? 5.f : 0.f);
-    while(1)
-    {
-        //System::Delay(500);
-        System::Delay(500);
-        phs += 0.01f;
-        if(phs > 1.f)
-            phs -= 1.f;
-        // patch.WriteCvOut(patch.CV_OUT_BOTH, phs * 5.f);
-        patch.WriteCvOut(patch.CV_OUT_BOTH, ledstate ? 5.f : 0.f);
-        patch.SetLed(ledstate);
-        if(ledstate)
-            ledstate = false;
-        else
-            ledstate = true;
-    }
+
+    /** And the new stuff for gpio as well */
+
+    while(1) {}
 }
