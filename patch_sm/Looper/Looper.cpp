7d691facdc114736ba49fe4f6440af9b3d792da5
beserge <bensergentanis@gmail.com>
2021-10-27 16:30:57 -0400

    Simple Looper setup
    Controls for in level, loop level, record on off
    TODO long press button to clear loop

--- patch_sm/Looper/Looper.cpp
+++ patch_sm/Looper/Looper.cpp
@@ -5,21 +5,56 @@ using namespace daisy;
 using namespace patch_sm;
 using namespace daisysp;
 
-DaisyPatchSM hw;
+DaisyPatchSM patch;
+Switch       button;
 
-void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
+Looper<48000 * 10>               looper_l;
+Looper<48000 * 10>               looper_r;
+Buffer<48000 * 10> DSY_SDRAM_BSS buffer_l;
+Buffer<48000 * 10> DSY_SDRAM_BSS buffer_r;
+
+float loop_level, in_level;
+
+void AudioCallback(AudioHandle::InputBuffer  in,
+                   AudioHandle::OutputBuffer out,
+                   size_t                    size)
 {
-	hw.ProcessAllControls();
-	for (size_t i = 0; i < size; i++)
-	{
-		OUT_L[i] = IN_L[i];
-		OUT_R[i] = IN_R[i];
-	}
+    patch.ProcessAllControls();
+    for(size_t i = 0; i < size; i++)
+    {
+        float in_l = IN_L[i] * in_level;
+        float in_r = IN_R[i] * in_level;
+
+        float sig_l = looper_l.Process(in_l) * loop_level + in_l;
+        float sig_r = looper_r.Process(in_r) * loop_level + in_r;
+
+        OUT_L[i] = sig_l;
+        OUT_R[i] = sig_r;
+    }
 }
 
 int main(void)
 {
-	hw.Init();
-	hw.StartAudio(AudioCallback);
-	while(1) {}
+    patch.Init();
+
+    looper_r.Init(&buffer_l);
+    looper_r.Init(&buffer_r);
+
+    button.Init(patch.B7);
+
+    patch.StartAudio(AudioCallback);
+    while(1)
+    {
+        patch.ProcessAllControls();
+        button.Debounce();
+
+        in_level   = patch.controls[0].Value();
+        loop_level = patch.controls[1].Value();
+
+        if(button.RisingEdge())
+        {
+            looper_l.TrigRecord();
+            looper_r.TrigRecord();
+        }
+    }
 }
