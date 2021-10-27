ae5759d495237e1cf835a6c332a779b8c8b99899
beserge <bensergentanis@gmail.com>
2021-10-27 19:29:37 +0000

    Automated: committing clang-format changes

--- patch_sm/Looper/Looper.cpp
+++ patch_sm/Looper/Looper.cpp
@@ -7,19 +7,21 @@ using namespace daisysp;
 
 DaisyPatchSM hw;
 
-void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
+void AudioCallback(AudioHandle::InputBuffer  in,
+                   AudioHandle::OutputBuffer out,
+                   size_t                    size)
 {
-	hw.ProcessAllControls();
-	for (size_t i = 0; i < size; i++)
-	{
-		OUT_L[i] = IN_L[i];
-		OUT_R[i] = IN_R[i];
-	}
+    hw.ProcessAllControls();
+    for(size_t i = 0; i < size; i++)
+    {
+        OUT_L[i] = IN_L[i];
+        OUT_R[i] = IN_R[i];
+    }
 }
 
 int main(void)
 {
-	hw.Init();
-	hw.StartAudio(AudioCallback);
-	while(1) {}
+    hw.Init();
+    hw.StartAudio(AudioCallback);
+    while(1) {}
 }
