814e04cf5c2e5afeacdb2423e70bab2eb8bdda45
beserge <bensergentanis@gmail.com>
2021-10-27 15:25:16 -0400

    Create patch sm Looper

---
+++ patch_sm/Looper/Looper.cpp
@@ -0,0 +1,25 @@
+#include "daisy_patch_sm.h"
+#include "daisysp.h"
+
+using namespace daisy;
+using namespace patch_sm;
+using namespace daisysp;
+
+DaisyPatchSM hw;
+
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
+{
+	hw.ProcessAllControls();
+	for (size_t i = 0; i < size; i++)
+	{
+		OUT_L[i] = IN_L[i];
+		OUT_R[i] = IN_R[i];
+	}
+}
+
+int main(void)
+{
+	hw.Init();
+	hw.StartAudio(AudioCallback);
+	while(1) {}
+}
