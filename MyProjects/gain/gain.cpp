802cbb33767d008c974493659bdb0ba504e9637a
Riley Wong <rileynwong@gmail.com>
2023-02-01 20:28:33 -0500

    Create directory for custom DSP

---
+++ MyProjects/gain/gain.cpp
@@ -0,0 +1,25 @@
+#include "daisy_seed.h"
+#include "daisysp.h"
+
+using namespace daisy;
+using namespace daisysp;
+
+DaisySeed hw;
+
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		out[0][i] = in[0][i];
+		out[1][i] = in[1][i];
+	}
+}
+
+int main(void)
+{
+	hw.Init();
+	hw.SetAudioBlockSize(4); // number of samples handled per callback
+	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+	hw.StartAudio(AudioCallback);
+	while(1) {}
+}
