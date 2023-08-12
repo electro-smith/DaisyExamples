1710c51b7b64dd8400d18e7189807b17606e664a
Isaac Pearl <pearl.isaac@gmail.com>
2023-08-12 19:23:48 -0400

    button example

--- seed/Button/Button.cpp
+++ seed/Button/Button.cpp
@@ -17,16 +17,31 @@ int main(void)
 
     //Configure and initialize button
     Switch button1;
+    Switch button2;
+    Switch button3;
+    Switch button4;
+    Switch button5;
     //Set button to pin 28, to be updated at a 1kHz  samplerate
+    // make thsi an array to loop through later
     button1.Init(hw.GetPin(28), 1000);
+    button2.Init(hw.GetPin(27), 1000);
+    button3.Init(hw.GetPin(26), 1000);
+    button4.Init(hw.GetPin(25), 1000);
+    button5.Init(hw.GetPin(24), 1000);
 
     // Loop forever
     for(;;)
     {
         //Debounce the button
         button1.Debounce();
+        button2.Debounce();
+        button3.Debounce();
+        button4.Debounce();
+        button5.Debounce();
         //If the button is pressed, turn the LED on
-        hw.SetLed(button1.Pressed());
+        // add if block to determine which button status to pass to setled
+        bool buttonPressed = button1.Pressed() || button2.Pressed() || button3.Pressed() || button4.Pressed() || button5.Pressed();
+        hw.SetLed(buttonPressed);
         //wait 1 ms
         System::Delay(1);
     }
