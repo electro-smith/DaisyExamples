9f0d13c756e02b6000b1e9643ba0da159007115f
beserge <bensergentanis@gmail.com>
2024-03-06 21:04:22 +0000

    Automated: committing clang-format changes

--- patch/ScrollingFonts/ScrollingFonts.cpp
+++ patch/ScrollingFonts/ScrollingFonts.cpp
@@ -24,11 +24,26 @@ int main(void)
     // Init
     hw.Init();
 
-    FontDef* fonts[] = {&Font_4x6, &Font_4x8, &Font_5x8, &Font_6x7, &Font_6x8, &Font_7x10, &Font_11x18, &Font_16x26};
-    std::string names[] = {"Font_4x6", "Font_4x8", "Font_5x8", "Font_6x7", "Font_6x8", "Font_7x10", "Font_11x18", "16x26"};
+    FontDef*    fonts[] = {&Font_4x6,
+                        &Font_4x8,
+                        &Font_5x8,
+                        &Font_6x7,
+                        &Font_6x8,
+                        &Font_7x10,
+                        &Font_11x18,
+                        &Font_16x26};
+    std::string names[] = {"Font_4x6",
+                           "Font_4x8",
+                           "Font_5x8",
+                           "Font_6x7",
+                           "Font_6x8",
+                           "Font_7x10",
+                           "Font_11x18",
+                           "16x26"};
 
     //display
-    std::string str  = " A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \
+    std::string str
+        = " A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \
      a b c d e f g h i j k l m n o p q r s t u v w x y z \
      0 1 2 3 4 5 6 7 8 9 \
      ! \" # $ % & ' () * + , - . / : ; \
@@ -37,7 +52,7 @@ int main(void)
     // Start stuff.
     hw.StartAudio(AudioCallback);
 
-    size_t x = 0;
+    size_t x    = 0;
     size_t font = 7;
 
     for(;;)
